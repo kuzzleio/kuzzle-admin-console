b2258f960c3741c5a87723dd33d7391a4b393048
Luca Marchesini <luca@kuzzle.io>
2015-12-07 16:16:15 +0100

    MessageLog is now able to clear itself.

--- public/javascripts/realtime/messageLog/messageLog.directive.js
+++ public/javascripts/realtime/messageLog/messageLog.directive.js
@@ -1,11 +1,16 @@
 angular.module('kuzzle.realtime')
-
+  .controller('messageLogCtrl', ['$scope', function ($scope) {
+    $scope.clearMessages = function () {
+      $scope.messages = [];
+    }
+  }])
   .directive('messages', function () {
     return {
       restrict: 'E',
       scope: {
         messages: "="
       },
+      controller: 'messageLogCtrl',
       templateUrl: '/javascripts/realtime/messageLog/messageLog.tpl.html'
     }
   });
