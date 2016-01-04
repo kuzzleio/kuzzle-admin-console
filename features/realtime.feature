affc29d01488df3da6879ebea484e425c8b08498
Luca Marchesini <luca@kuzzle.io>
2016-01-04 18:44:48 +0100

    Tests for updates, deletes, volatiles and user notifications

--- features/realtime.feature
+++ features/realtime.feature
@@ -6,33 +6,62 @@ Feature: Test realtime watch data pages
   Background:
     Given I go to the login page
     And I authenticate
-
-  @cleanDb
-  Scenario: I can subscribe to a collection
     Given I go to the realtime page
-    Then I see the collection selector
     And I click on the collection selector
-    Then I see my collections
     Given I click on a collection
     And I subscribe to the collection events
+
+  @cleanDb @unsubscribe
+  Scenario: I can subscribe to a collection
     Then I see that I am subscribed
 
-  @cleanDb
+  @cleanDb @unsubscribe
+  Scenario: I can clear the message log
+    Given I clear the message log
+    Then The message log is empty
+
+  @cleanDb @unsubscribe
   Scenario: Once subscribed, I receive notifications about document creations
-    Given I go to the realtime page
-    And I click on the collection selector
-    Given I click on a collection
-    And I subscribe to the collection events
+    Given I clear the message log
+    And I create a persistent document
+    Then I receive the notification that the document has been created
+
+  @cleanDb @unsubscribe
+  Scenario: Once subscribed, I receive notifications about document updates
+    Given I create a persistent document
+    And I clear the message log
+    And I update a persistent document
+    Then I receive the notification that the document has been updated
+
+  @cleanDb @unsubscribe
+  Scenario: Once subscribed, I receive notifications about document deletions
+    Given I create a persistent document
+    And I clear the message log
+    And I delete a persistent document
+    Then I receive the notification that the document has been deleted
+
+  @cleanDb @unsubscribe
+  Scenario: Once subscribed, I receive notifications about volatile messages
+    Given I clear the message log
+    And I publish a volatile message
+    Then I receive the notification about the volatile message
+
+  @cleanDb @unsubscribe
+  Scenario: Once subscribed, I receive notifications about users joining the room
+    Given I clear the message log
+    And Someone subscribes to my room
+    Then I receive the notification about the new user entering the room
+
+  @cleanDb @unsubscribe
+  Scenario: Once subscribed, I receive notifications about users leaving the room
+    Given Someone subscribes to my room
+    And I clear the message log
+    And Someone unsubscribes from the room
+    Then I receive the notification about the user leaving the room
+
+  @cleanDb
+  Scenario: Once unsubscribed, I do not receive any notification
+    Given I unsubscribe from the collection
     And I clear the message log
     And I create a persistent document
-    Then I receive the notification that a new document has been created
-
-  Scenario: Once subscribed, I receive notifications about document updates
-
-  Scenario: Once subscribed, I receive notifications about document deletions
-
-  Scenario: Once subscribed, I receive notifications about volatile messages
-
-  Scenario: Once subscribed, I receive notifications about users joining the room
-
-  Scenario: Once subscribed, I receive notifications about users leaving the room
+    Then The message log is empty
--- features/step_definitions/realtime/realtime.js
+++ features/step_definitions/realtime/realtime.js
@@ -1,7 +1,9 @@
 var
   request = require('request'),
+  Kuzzle = require('kuzzle-sdk'),
   fixtures = require('../../fixtures.json'),
   config = require('../../support/config.js'),
+  kuzzleUrl = 'http://' + config.kuzzleHost + ':' + config.kuzzlePort,
   testDocument = {
     type: 'testDocument',
     text: 'yo!'
@@ -9,8 +11,7 @@ var
   updatedTestDocument = {
     type: 'updatedTestDocument',
     text: 'Hi!'
-  },
-  kuzzleUrl = 'http://' + config.kuzzleHost + ':' + config.kuzzlePort;
+  };
 
 module.exports = function () {
   this.Given(/^I go to the realtime page$/, function (callback) {
@@ -37,12 +38,62 @@ module.exports = function () {
     this.browser.pressButton('.filters button.btn-subscribe', callback);
   });
 
+  this.Given(/^I unsubscribe from the collection$/, {timeout: 20 * 1000}, function (callback) {
+    this.browser.pressButton('.filters button.btn-unsubscribe', callback);
+  });
+
   this.Then(/^I see that I am subscribed$/, function () {
     this.browser.assert.text('messages ul.message-list li.message-item:last-child code.document-id', this.collection);
   });
 
   this.Given(/^I clear the message log$/, {timeout: 20 * 1000}, function (callback) {
     this.browser.pressButton('messages button.btn-clear', callback);
+  });
+
+  this.Then(/^The message log is empty$/, function () {
+    this.browser.assert.elements('messages ul.message-list li.message-item', {exactly: 0});
+  });
+
+  this.Then(/^I receive the notification that the document has been created$/, function () {
+    if (!this.currentDocumentId) {
+      callback(new Error('Expected to have the id of the current document'));
+      return;
+    }
+
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-created-updated-doc', 'Created new document');
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child code.document-id', this.currentDocumentId);
+  });
+
+  this.Then(/^I receive the notification that the document has been updated$/, function () {
+    if (!this.currentDocumentId) {
+      callback(new Error('Expected to have the id of the current document'));
+      return;
+    }
+
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-created-updated-doc', 'Updated document');
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child code.document-id', this.currentDocumentId);
+  });
+
+  this.Then(/^I receive the notification that the document has been deleted$/, function () {
+    if (!this.currentDocumentId) {
+      callback(new Error('Expected to have the id of the current document'));
+      return;
+    }
+
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-deleted-doc', 'Deleted document');
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child code.document-id', this.currentDocumentId);
+  });
+
+  this.Then(/^I receive the notification about the volatile message$/, function () {
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-volatile', 'Received volatile message');
+  });
+
+  this.Then(/^I receive the notification about the new user entering the room$/, function () {
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-user', 'A new user is listening to this room');
+  });
+
+  this.Then(/^I receive the notification about the user leaving the room$/, function () {
+    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-user', 'A user exited this room');
   });
 
   this.Given(/^I create a persistent document$/, {timeout: 20 * 1000}, function (callback) {
@@ -53,9 +104,61 @@ module.exports = function () {
       uri: url,
       body: testDocument,
       json: true
+    }, (error, response, body) => {
+      if (error){
+        console.log('Error creating new document on  '+ url + ': ' + error);
+        callback(new Error('Error creating new document on  '+ url + ': ' + error));
+        return;
+      }
+
+      this.currentDocumentId = body.result._id;
+
+      setTimeout(() => {
+        callback();
+      }, 1000);
+    });
+  });
+
+  this.Given(/^I publish a volatile message$/, {timeout: 20 * 1000}, function (callback) {
+    var url = kuzzleUrl + '/api/v1.0/' + this.index + '/' + this.collection;
+
+    request({
+      method: 'POST',
+      uri: url,
+      body: testDocument,
+      json: true
+    }, (error, response, body) => {
+      if (error){
+        console.log('Error publishing volatile message on  '+ url + ': ' + error);
+        callback(new Error('Error publishing volatile message on  '+ url + ': ' + error));
+        return;
+      }
+
+      setTimeout(() => {
+        callback();
+      }, 1000);
+    });
+  });
+
+  this.Given(/^I update a persistent document$/, {timeout: 20 * 1000}, function (callback) {
+    if (!this.currentDocumentId) {
+      callback(new Error('Expected to have the id of the current document'));
+      return;
+    }
+
+    var url = kuzzleUrl + '/api/v1.0/' + this.index + '/' + this.collection + '/' + this.currentDocumentId;
+
+    request({
+      method: 'PUT',
+      uri: url,
+      body: updatedTestDocument,
+      json: true
       }, (error, response, body) => {
-        if (error)
-          console.log('Error creating new document on  '+ url + ': ' + error);
+        if (error){
+          console.log('Error updating new document on  '+ url + ': ' + error);
+          callback(new Error('Error updating new document on  '+ url + ': ' + error));
+          return;
+        }
 
         setTimeout(() => {
           callback();
@@ -63,7 +166,44 @@ module.exports = function () {
       });
   });
 
-  this.Then(/^I receive the notification that a new document has been created$/, function () {
-    this.browser.assert.text('messages ul.message-list li.message-item:last-child span.message-created-updated-doc', 'Created new document');
+  this.Given(/^I delete a persistent document$/, {timeout: 20 * 1000}, function (callback) {
+    if (!this.currentDocumentId) {
+      callback(new Error('Expected to have the id of the current document'));
+      return;
+    }
+
+    var url = kuzzleUrl + '/api/v1.0/' + this.index + '/' + this.collection + '/' + this.currentDocumentId;
+
+    request({
+      method: 'DELETE',
+      uri: url
+      }, (error, response, body) => {
+        if (error){
+          console.log('Error deleting new document on  '+ url + ': ' + error);
+          callback(new Error('Error deleting new document on  '+ url + ': ' + error));
+          return;
+        }
+
+        setTimeout(() => {
+          callback();
+        }, 1000);
+      });
+  });
+
+  this.Given(/^Someone subscribes to my room$/, function (callback) {
+    this.currentRoom = this.kuzzle.dataCollectionFactory(this.collection).subscribe({}, function (error, result) {});
+
+    setTimeout(() => {
+      callback();
+    }, 200);
+  });
+
+  this.Given(/^Someone unsubscribes from the room$/, function (callback) {
+    this.currentRoom.unsubscribe();
+
+    setTimeout(() => {
+      this.currentRoom = null;
+      callback();
+    }, 200);
   });
 };
