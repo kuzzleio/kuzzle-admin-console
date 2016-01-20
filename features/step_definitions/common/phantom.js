e77cc851ef21b108c8fe25e0a4810f8bdbba743f
Anthony Sendra <asendra@kaliop.com>
2016-01-20 16:44:16 +0100

    clean phantom use

--- features/step_definitions/common/phantom.js
+++ features/step_definitions/common/phantom.js
@@ -1,27 +1,30 @@
-var fs = require('fs');
-
 module.exports = function () {
   this.World = require('../../support/world.js').World;
 
-  this.Given(/^I go to the login page$/, function (callback) {
-    this.visit('#/login', callback);
+  this.Given(/^I go to the login page$/, function () {
+    return this.visit('#/login');
   });
 
-  this.Then(/^I have a login button$/, function(callback) {
-    this.browser.waitForElementByCss('button[type="submit"]', 5000, function(err, el) {
-      if (err) {
-        callback.fail();
-      } else {
-        this.browser.takeScreenshot(function (error, screenshot) {
-          // console.log("screenshot taken", error, screenshot);
-          fs.writeFile('logo.png', screenshot, 'base64', function(err){
-            if (err) throw err
-            console.log('File saved.')
-            callback();
-          });
-          // this.browser.quit();
-        });
-      }
-    }.bind(this));
+  this.Then(/^I have a login button$/, function() {
+    //this.browser.waitForElementByCss('button[type="submit"]', 5000, function(err, el) {
+    //  if (err) {
+    //    callback.fail();
+    //  } else {
+    //    this.browser.takeScreenshot(function (error, screenshot) {
+    //      // console.log("screenshot taken", error, screenshot);
+    //      fs.writeFile('logo.png', screenshot, 'base64', function(err){
+    //        if (err) throw err
+    //        console.log('File saved.')
+    //        callback();
+    //      });
+    //      // this.browser.quit();
+    //    });
+    //  }
+    //}.bind(this));
+
+    return this.browser.waitForElementByCss('button[type="submit"]', 5000)
+      .then(() => {
+        return this.takeScreenshot('login-button');
+      })
   });
 };
--- features/support/world.js
+++ features/support/world.js
@@ -1,20 +1,24 @@
 var
   wd = require('wd'),
-  remote = wd.remote(), // this.browser will be available in step definitions
+  remote = wd.promiseRemote(), // this.browser will be available in step definitions
   fixtures = require('../fixtures.json'),
   config = require('./config.js'),
   Kuzzle = require('kuzzle-sdk'),
-  kuzzleUrl = 'http://' + config.kuzzleHost + ':' + config.kuzzlePort;
+  kuzzleUrl = 'http://' + config.kuzzleHost + ':' + config.kuzzlePort,
+  q = require('q'),
+  fs = require('fs');
 
 // log status output from web driver
-remote.on('status', function(info){
-  console.log('\x1b[36m%s\x1b[0m', info);
-});
+if (process.env.DEBUG_PHANTOM) {
+  remote.on('status', function(info){
+    console.log('\x1b[36m%s\x1b[0m', info);
+  });
 
-// log commands from web driver
-remote.on('command', function(meth, path, data){
-  console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path, data || '');
-});
+  // log commands from web driver
+  remote.on('command', function(meth, path, data){
+    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path, data || '');
+  });
+}
 
 exports.World = function World(callback) {
   this.browser = remote;
@@ -25,14 +29,29 @@ exports.World = function World(callback) {
   this.currentRoom = undefined;
   this.kuzzle = new Kuzzle(kuzzleUrl, this.index);
 
-  this.visit = function (url, callback) {
-    this.browser.get(this.baseUrl + url, callback);
+  // run the callback when we are done do cucumber knows we are ready
+  this.browser.init({browserName: 'firefox'})
+    .then(() => {
+      return this.browser.windowSize('current', 1024, 768);
+    })
+    .then(() => {
+      callback();
+    });
+
+
+  /* DEFINE SHORTCUTS */
+  this.visit = function (url) {
+    return this.browser.get(this.baseUrl + url);
   };
 
-  // run the callback when we are done do cucumber knows we are ready
-  this.browser.init({browserName: 'firefox', viewportSize: {width: 1024, height: 768}}, function() {
-    this.browser.windowSize('current', 1024, 768, function () {
-      callback();
-    })
-  }.bind(this));
+  this.takeScreenshot = function (id) {
+    if (!id) {
+      id = Math.random();
+    }
+
+    return this.browser.takeScreenshot()
+      .then((data) => {
+        return q.ninvoke(fs, 'writeFile', ['features/screenshots/' + id + '.png', data, 'base64']);
+      });
+  }
 };
