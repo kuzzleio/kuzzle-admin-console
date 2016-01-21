80392ee6667a7f3fb1632e8281a623167551612b
Anthony Sendra <asendra@kaliop.com>
2016-01-21 17:12:29 +0100

    migrate form to wdio

--- features/step_definitions/common/forms.js
+++ features/step_definitions/common/forms.js
@@ -1,17 +1,22 @@
 module.exports = function () {
   this.Then(/^I fill the input "([^"]*)" with "([^"]*)"$/, function (id, value) {
-    this.browser.fill('#' + id, value);
+    browser
+      .setValue('#' + id, value)
+      .call(callback)
   });
 
   this.Then(/^I click on "([^"]*)" button$/, {timeout: 20 * 1000}, function (id, callback) {
-    this.browser.pressButton('#' + id, () => {
-      setTimeout(() => {
-        callback();
-      }, 2000);
-    });
+    browser
+      .click('#' + id)
+      .call(callback)
   });
 
   this.Then(/^the field "([^"]*)" has the value "([^"]*)"$/, function (id, value) {
-    this.browser.assert.input('#' + id, value)
+    browser
+      .getValue('#' + id)
+      .then((inputValue) => {
+        assert.equal(inputValue, value, 'The value ' + value + ' is not present in field ' + id);
+      })
+      .call(callback);
   });
 };
