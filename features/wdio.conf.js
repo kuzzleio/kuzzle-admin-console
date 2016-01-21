70b9332fb7b69b7f457c50012ee2ae30a0e09de3
Anthony Sendra <asendra@kaliop.com>
2016-01-21 15:59:24 +0100

    change path in wdio conf

--- features/wdio.conf.js
+++ features/wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './authentication.feature'
+        './features/authentication.feature'
     ],
     // Patterns to exclude.
     exclude: [
