c2821d8a2565d43f15541100c292d0892c37be72
Luca Marchesini <luca@kuzzle.io>
2016-02-11 17:35:15 +0100

    Added canDeleteCollection right check

--- public/javascripts/common/authentication/authorizationApi.service.js
+++ public/javascripts/common/authentication/authorizationApi.service.js
@@ -40,12 +40,36 @@ angular.module('kuzzle.authorization', [])
         canDeleteIndex: function (index) {
         },
         canDeleteCollection: function (index, collection) {
+          var userProfile = session.user.content.profile;
+          if (!index || !collection) {
+            throw '[canDeleteCollection] Missing argument';
+          }
+
+          if (!hasUser(session.user) || !hasRole(session.user)) {
+            return false;
+          }
+
+          return userProfile.content.roles.reduce(function(accumulator, role) {
+            var roleIndexes = role.content.indexes;
+            var roleHasIndex = Object.keys(roleIndexes).reduce(function(indexAccumulator, indexIdentifier) {
+              if ((index === indexIdentifier || indexIdentifier === '*' && index !== kuzzleCoreIndex)) {
+                var indexCollections = roleIndexes[indexIdentifier].collections;
+                var collectionHasDelete = Object.keys(indexCollections).reduce(function (collectionAccumulator, collectionIdentifier) {
+                    if (collection === collectionIdentifier || collectionIdentifier === '*') {
+                      return collectionAccumulator || !!indexCollections[collectionIdentifier]._canDelete;
+                    }
+                    return collectionAccumulator;
+                }, false);
+                return indexAccumulator || !!collectionHasDelete;
+              }
+              return indexAccumulator;
+            },false);
+            return accumulator || !!roleHasIndex;
+          }, false);
         },
         canUseController: function (index, collection, controller) {
         },
         canDoAction: function (index, collection, controller, action) {
-
-
         }
       };
     }]);
