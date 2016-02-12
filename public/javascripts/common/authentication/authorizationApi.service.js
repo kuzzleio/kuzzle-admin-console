dbc33a03cae5fc7f74b4ead377965a97177f505f
David Bengsch <dbengsch@kaliop.com>
2016-02-12 11:36:59 +0100

    KUZBO-75 : Finalize authorization service

--- public/javascripts/common/authentication/authorizationApi.service.js
+++ public/javascripts/common/authentication/authorizationApi.service.js
@@ -33,8 +33,11 @@ angular.module('kuzzle.authorization', [])
           return session.user.content.profile.content.roles.reduce(function(accumulator, role) {
             var roleIndexes = role.content.indexes;
             var roleHasCollectionCreate = Object.keys(roleIndexes).reduce(function (indexAccumulator, indexIdentifier) {
-              if ((index === indexIdentifier || indexIdentifier === '*' && index !== kuzzleCoreIndex)) {
-
+              if ((index === indexIdentifier ||
+                indexIdentifier === '*' &&
+                index !== kuzzleCoreIndex) &&
+                indexIdentifier.charAt(0) !== '_'
+              ) {
                 return indexAccumulator || !!roleIndexes[indexIdentifier].collections._canCreate;
               }
               return indexAccumulator;
@@ -44,22 +47,118 @@ angular.module('kuzzle.authorization', [])
           }, false);
         },
         canDeleteIndex: function (index) {
+          if (!index) {
+            throw new Error('[canDeleteIndex] Missing argument');
+          }
+
+          if (!hasUser(session.user) || !hasRole(session.user)) {
+            return false;
+          }
+
+          return session.user.content.profile.content.roles.reduce(function(accumulator, role) {
+            var roleIndexes = role.content.indexes;
+            var roleHasCollectionCreate = Object.keys(roleIndexes).reduce(function (indexAccumulator, indexIdentifier) {
+              var roleIndex = roleIndexes[indexIdentifier];
+              if ((index === indexIdentifier ||
+                indexIdentifier === '*' &&
+                index !== kuzzleCoreIndex) &&
+                indexIdentifier.charAt(0) !== '_'
+              ) {
+                return indexAccumulator || !!roleIndex._canDelete;
+              }
+              return indexAccumulator;
+            }, false);
+
+            return accumulator || roleHasCollectionCreate;
+          }, false);
         },
         canDeleteCollection: function (index, collection) {
-        },
-        canUseController: function (index, collection, controller) {
+          if (!index || !collection) {
+            throw new Error('[canDeleteCollection] Missing argument');
+          }
+
+          if (!hasUser(session.user) || !hasRole(session.user)) {
+            return false;
+          }
+
+          return session.user.content.profile.content.roles.reduce(function(accumulator, role) {
+            var roleIndexes = role.content.indexes;
+            var roleHasCollectionCreate = Object.keys(roleIndexes).reduce(function (indexAccumulator, indexIdentifier) {
+              var roleIndex = roleIndexes[indexIdentifier];
+              if ((index === indexIdentifier ||
+                indexIdentifier === '*' &&
+                index !== kuzzleCoreIndex) &&
+                indexIdentifier.charAt(0) !== '_'
+              ) {
+                var hasCollectionDelete = Object.keys(roleIndex.collections).reduce(function (collectionAccumulator, collectionIdentifier) {
+                  if (collection === collectionIdentifier || collectionIdentifier === '*') {
+                    return collectionAccumulator || !!roleIndex.collections[collectionIdentifier]._canDelete;
+                  }
+                  return collectionAccumulator;
+                }, false);
+
+                return indexAccumulator || hasCollectionDelete;
+              }
+              return indexAccumulator;
+            }, false);
+
+            return accumulator || roleHasCollectionCreate;
+          }, false);
         },
         canDoAction: function (index, collection, controller, action) {
+          if (!index || !collection || !controller || !action) {
+            throw new Error('[canDeleteCollection] Missing argument');
+          }
 
+          if (!hasUser(session.user) || !hasRole(session.user)) {
+            return false;
+          }
 
+          return session.user.content.profile.content.roles.reduce(function(accumulator, role) {
+            var roleIndexes = role.content.indexes;
+            var roleHasCollectionCreate = Object.keys(roleIndexes).reduce(function (indexAccumulator, indexIdentifier) {
+              var roleIndex = roleIndexes[indexIdentifier];
+              if ((index === indexIdentifier ||
+                indexIdentifier === '*' &&
+                index !== kuzzleCoreIndex) &&
+                indexIdentifier.charAt(0) !== '_'
+              ) {
+                var hasCollectionRight = Object.keys(roleIndex.collections).reduce(function (collectionAccumulator, collectionIdentifier) {
+                  var currentCollection = roleIndex.collections[collectionIdentifier];
+                  if ((collection === collectionIdentifier ||
+                    collectionIdentifier === '*') &&
+                    collectionIdentifier.charAt(0) !== '_'
+                  ) {
+                    var hasControllerRight = Object.keys(currentCollection.controllers).reduce(function (controllerAccumulator, controllerIdentifier) {
+                      var currentController = currentCollection.controllers[controllerIdentifier];
+                      if (controller === controllerIdentifier || controllerIdentifier === '*') {
+                        var hasActionRight = Object.keys(currentController.actions).reduce(function (actionAccumulator, actionIdentifier) {
+                          if ((action === actionIdentifier || actionIdentifier === '*')) {
+                            if (typeof currentController.actions[actionIdentifier] === 'function') {
+                              return true;
+                            }
+                            return actionAccumulator || !!currentController.actions[actionIdentifier];
+                          }
+                          return controllerAccumulator;
+                        }, false);
+                        return controllerAccumulator || !!hasActionRight;
+                      }
+                      return controllerAccumulator;
+                    }, false);
+                    return collectionAccumulator || hasControllerRight;
+                  }
+                  return collectionAccumulator;
+                }, false);
+                return indexAccumulator || hasCollectionRight;
+              }
+              return indexAccumulator;
+            }, false);
+            return accumulator || roleHasCollectionCreate;
+          }, false);
         }
       };
     }]);
 
 /*
-content.roles[...].content.indexes.canCreate
-content.roles[...].content.indexes[...].canDelete
- content.roles[...].content.indexes[...].collections.canCreate
- content.roles[...].content.indexes[...].collections[...].canDelete
 content.roles[...].content.indexes[...].collections[...].controllers[...].actions[...]
 */
