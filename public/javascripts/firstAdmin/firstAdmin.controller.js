3ef904e2c47edf26572433b7bff73a4347edfc89
Jean-François Vial <jvial@kaliop.com>
2016-02-18 16:56:07 +0100

    comply with grunt-contrib-jshint bug

--- public/javascripts/firstAdmin/firstAdmin.controller.js
+++ public/javascripts/firstAdmin/firstAdmin.controller.js
@@ -26,41 +26,43 @@ angular.module('kuzzle.firstAdmin')
   '$state',
   'FirstAdminService',
   function ($scope, $window, $rootScope, $state, FirstAdminService) {
-  $scope.credentials = {
-    username: '',
-    password_a: '',
-    password_b: ''
-  };
 
-  $scope.createFirstAdmin = function (credentials) {
-    if (!credentials || !credentials.username || !credentials.password_a) {
-      $scope.message = 'Please, provide a valid username and password.';
-      return;
-    }
+    $scope.credentials = {
+      username: '',
+      passworda: '',
+      passwordb: ''
+    };
 
-    credentials.username = credentials.username.trim();
+    $scope.createFirstAdmin = function (credentials) {
+      if (!credentials || !credentials.username || !credentials.passworda) {
+        $scope.message = 'Please, provide a valid username and password.';
+        return;
+      }
 
-    if (credentials.username.length < 4) {
-      $scope.message = 'The username is way too short... consider to use a 4 chars long username and with no space in it.';
-      return;
-    }
+      credentials.username = credentials.username.trim();
 
-    if (credentials.password_a !== credentials.password_b) {
-      $scope.message = 'The password and its confirmation does not match... please, retry.';
-      return;
-    }
+      if (credentials.username.length < 4) {
+        $scope.message = 'The username is way too short... consider to use a 4 chars long username and with no space in it.';
+        return;
+      }
 
-    if (credentials.password_a.replace('/ /g', '') !== credentials.password_a || credentials.password_a.trim() !== credentials.password_a || credentials.password_a.length < 8) {
-      $scope.message = 'The password looks to not be valid.<br>Please avoid to use spaces or tabs and to type at least 8 chars.';
-      return;
-    }
+      if (credentials.passworda !== credentials.passwordb) {
+        $scope.message = 'The password and its confirmation does not match... please, retry.';
+        return;
+      }
 
-    FirstAdminService.create({username: credentials.username, password: credentials.password_a}).then(function (user) {
-      $window.location.reload();
-    },
-    function (error) {
-      $scope.message = 'Something really wrong just happend... look at the console...';
-      console.log(error);
-    });
-  };
-}]);
+      if (credentials.passworda.replace('/ /g', '') !== credentials.passworda || credentials.passworda.trim() !== credentials.passworda || credentials.passworda.length < 8) {
+        $scope.message = 'The password looks to not be valid.<br>Please avoid to use spaces or tabs and to type at least 8 chars.';
+        return;
+      }
+
+      FirstAdminService.create({username: credentials.username, password: credentials.passworda}).then(function (user) {
+        $window.location.reload();
+      },
+      function (error) {
+        $scope.message = 'Something really wrong just happend... look at the console...';
+        console.log(error);
+      });
+    };
+  }
+]);
