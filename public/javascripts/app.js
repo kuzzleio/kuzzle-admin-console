0d47bcf66da52a9e29feb8f12fd7e0f7c0a0c746
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-02-23 21:12:06 +0100

    eslint fix

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -81,7 +81,7 @@ angular.module('kuzzle', [
                     })
                     .catch(function () {
                       deferred.reject();
-                    })
+                    });
                 });
               });
 
