842755c2ff7b6ac0962f3896c0b8e4b8bdece441
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-02-23 20:58:50 +0100

    fix login & modal relog

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -66,10 +66,26 @@ angular.module('kuzzle', [
               '/javascripts/common/widget/widget.directive.js'
             ]);
           }],
-          authenticated: ['AuthService', 'kuzzleSdk', function (Auth, kuzzle) {
-            kuzzle.addListener('connected', function() {
-              Auth.isAuthenticated();
-            });
+          authenticated: ['AuthService', '$q', 'kuzzleSdk', function (Auth, q, kuzzle) {
+            var deferred = q.defer();
+
+            Auth.isAuthenticated()
+              .then(function () {
+                deferred.resolve();
+              })
+              .catch(function () {
+                kuzzle.addListener('connected', function() {
+                  Auth.isAuthenticated()
+                    .then(function () {
+                      deferred.resolve();
+                    })
+                    .catch(function () {
+                      deferred.reject();
+                    })
+                });
+              });
+
+            return deferred.promise;
           }]
         }
       })
