15587b663135983c45ee486a55cbb60c576b6050
Anthony Sendra <asendra@kaliop.com>
2016-02-25 16:23:29 +0100

    revert test for upload to s3

--- features/wdio.conf.js
+++ features/wdio.conf.js
@@ -24,16 +24,16 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    //'./features/authentication.feature',
+    './features/authentication.feature',
     './features/realtime.feature',
-    //'./features/browseData.feature',
-    //'./features/metrics.feature',
-    //'./features/indexes.feature',
-    //'./features/roles.feature',
-    //'./features/profiles.feature',
-    //'./features/users.feature',
-    //'./features/notAllowed.feature',
-    //'./features/collections.feature',
+    './features/browseData.feature',
+    './features/metrics.feature',
+    './features/indexes.feature',
+    './features/roles.feature',
+    './features/profiles.feature',
+    './features/users.feature',
+    './features/notAllowed.feature',
+    './features/collections.feature'
   ],
   // Patterns to exclude.
   exclude: [
