ce8aabef1be03a0f5927ce0ea8530ceb00176db2
Jeff <jeff@kuzzle.io>
2016-03-04 10:22:59 +0100

    cleanup

--- features/wdio.conf.js
+++ features/wdio.conf.js
@@ -63,13 +63,13 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: silent | verbose | command | data | result | error
-  logLevel: 'verbose',
+  logLevel: 'error',
   //
   // Enables colors for log output.
   coloredLogs: true,
   //
   // Saves a screenshot to a given path if a command fails.
-  //screenshotPath: './features/errorShots/',
+  screenshotPath: './features/errorShots/',
   //
   // Set a base URL in order to shorten url command calls. If your url parameter starts
   // with "/", the base url gets prepended.
