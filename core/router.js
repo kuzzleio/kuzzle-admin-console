b74d30bffe498d12a060d414d5b34682d076a84b
Luca Marchesini <luca@kuzzle.io>
2016-03-16 13:00:50 +0100

    Cleanup useless backend routes

--- core/router.js
+++ core/router.js
@@ -1,22 +1,14 @@
 var
   index = require('./controllers/index'),
   storage = require('./controllers/storage'),
-  collection = require('./controllers/collection'),
-  realtime = require('./controllers/realtime'),
-  login = require('./controllers/login'),
-  indexes = require('./controllers/indexes'),
-  metrics = require('./controllers/metrics');
+  login = require('./controllers/login');
 
 module.exports = {
 
   initRoutes: function (app) {
     app.use('/', index);
     app.use('/storage', storage);
-    app.use('/collection', collection);
-    app.use('/realtime', realtime);
     app.use('/login', login);
-    app.use('/indexes', indexes);
-    app.use('/metrics', metrics);
   }
 
 };
