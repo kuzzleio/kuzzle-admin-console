4b18c0dd4b066a038b674a306777225ca593d652
Luca Marchesini <luca@kuzzle.io>
2016-06-29 12:53:12 +0200

    Added TODO comments

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -6,13 +6,16 @@ export const doLogin = (store, username, password) => {
     .login('local', {username, password}, (err, res) => {
       if (err) {
         throw err.message
+        // TODO properly display error to the user
       }
-
+      // TODO properly get user information via whoAmI
       let user = {
         username,
         password
       }
+      // TODO store JWT in SessionStorage
       store.dispatch('SET_CURRENT_USER', user)
+      // TODO redirect to the previously asked route
       router.go('/')
     })
 }
