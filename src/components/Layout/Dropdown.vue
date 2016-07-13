ff8c2bd3abe1182e87b10962b19466c4dcd8887a
Anthony Sendra <asendra@kaliop.com>
2016-07-13 19:03:31 +0200

    fix redirect on click dropdown

--- src/components/Layout/Dropdown.vue
+++ src/components/Layout/Dropdown.vue
@@ -1,12 +1,18 @@
 <template>
   <span>
-    <a href="#" class="dropdown-button fa fa-ellipsis-v" data-activates="id"></a>
+    <a class="dropdown-button fa fa-ellipsis-v" data-activates="id"></a>
 
     <ul id="id" class='dropdown-content'>
       <slot></slot>
     </ul>
   </span>
 </template>
+
+<style lang="scss" rel="stylesheet/scss">
+  a.dropdown-button {
+    cursor: pointer;
+  }
+</style>
 
 <script>
   export default {
