0af52e5ec02ad60787556c729b41d11b882a16ed
Anthony Sendra <asendra@kaliop.com>
2016-07-22 13:47:13 +0200

    css search bar WIP

--- src/components/Common/Filters.vue
+++ src/components/Common/Filters.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <div class="row">
-      <div class="col s9 quick-search" v-if="!displayBlockFilter">
+      <div class="col s9 quick-search">
         <div class="row">
           <form>
-            <div class="col s5">
+            <div class="col s6">
               <div class="search-bar">
-                <i class="fa fa-search"></i>
+                <i class="fa fa-search search"></i>
                 <input type="text" placeholder="Search something..." @input="setSearchTerm" :value="searchTerm"/>
+                <a href="#" @click.prevent="displayBlockFilter = true">More query options</a>
                 <i class="fa fa-times remove-search" @click="resetSearchTerm"></i>
               </div>
             </div>
@@ -18,47 +19,74 @@
         </div>
       </div>
 
-      <div class="col s8 z-depth-1" v-if="displayBlockFilter">
+      <div class="col s8 z-depth-2 open-search" v-if="displayBlockFilter">
+        <i class="fa fa-times close" @click="displayBlockFilter = false"></i>
         <tabs @tab-changed="switchFilter" :active="tabActive">
-          <tab name="basic"><a href="">Basic filters</a></tab>
-          <tab name="advanced"><a href="#!/tabs/2">Advanced filters</a></tab>
+          <tab name="basic"><a href="">Basic Mode</a></tab>
+          <tab name="json"><a href="">Raw JSON Mode</a></tab>
 
-          <div slot="contents">
+          <div slot="contents" class="card">
             <div class="col s12 filter-content">
               <div v-if="tabActive === 'basic'">
                 <div class="row">
                   <div class="col s8">
-                    <div class="row">
-                      <div class="col s3">
-                        <input placeholder="Attribute" type="text" class="validate">
-                      </div>
-                      <div class="col s3">
-                        <select v-m-select>
-                          <option value="match">Match</option>
-                        </select>
-                      </div>
-                      <div class="col s3">
-                        <input placeholder="Placeholder" type="text" class="validate">
-                      </div>
-                      <div class="col s3">
-                        <button class="inline btn waves-effect waves-light"><i class="fa fa-plus left"></i>And</button>
+
+                    <div class="row block-and">
+                      <p><i class="fa fa-search"></i>Query</p>
+                      <div class="row dots block-content">
+                        <div class="col s4">
+                          <input placeholder="Attribute" type="text" class="validate">
+                        </div>
+                        <div class="col s4">
+                          <select v-m-select>
+                            <option value="match">Match</option>
+                          </select>
+                        </div>
+                        <div class="col s3">
+                          <input placeholder="Placeholder" type="text" class="validate">
+                        </div>
+                        <div class="col s3">
+                          <button class="inline btn waves-effect waves-light"><i class="fa fa-plus left"></i>And</button>
+                        </div>
                       </div>
                     </div>
-                    <hr/>
+
                     <div class="row">
                       <button class="btn waves-effect waves-light"><i class="fa fa-plus left"></i>Or</button>
                     </div>
-                    <hr/>
-                    <div class="row">
-                      <button class="btn waves-effect waves-light">Filter</button>
-                      <button class="btn-flat">Reset</button>
+
+                    <div class="row block-sort">
+                      <p><i class="fa fa-sort-amount-asc"></i>Sorting</p>
+                      <div class="row block-content">
+                        <div class="col s4">
+                          <input placeholder="Attribute" type="text" class="validate">
+                        </div>
+                        <div class="col s2">
+                          <select v-m-select>
+                            <option value="match">asc</option>
+                            <option value="match">desc</option>
+                          </select>
+                        </div>
+                      </div>
                     </div>
+
                   </div>
                 </div>
+                    <div class="row actions card-action">
+                      <button class="btn waves-effect waves-light">Search</button>
+                      <button class="btn waves-effect waves-light">Reset</button>
+                    </div>
               </div>
             </div>
           </div>
         </tabs>
+      </div>
+
+      <div
+        v-if="displayBlockFilter"
+        @click="displayBlockFilter = false"
+        class="lean-overlay"
+        style="z-index: 1000; display: block; opacity: 0;">
       </div>
     </div>
   </div>
@@ -71,6 +99,8 @@
   import { setSearchTerm, performSearch, resetSearchTerm } from '../../vuex/modules/collection/actions'
   import { searchUsers } from '../../vuex/modules/collection/users-actions'
   import { searchTerm } from '../../vuex/modules/collection/getters'
+
+  const ESC_KEY = 27
 
   export default {
     directives: {
@@ -93,7 +123,7 @@
     },
     data () {
       return {
-        displayBlockFilter: false,
+        displayBlockFilter: true,
         tabActive: 'basic'
       }
     },
@@ -104,24 +134,20 @@
       newSearch () {
         this.performSearch('users', '%kuzzle')
       }
+    },
+    ready () {
+      window.document.addEventListener('keydown', evt => {
+        evt = evt || window.event
+
+        if (evt.keyCode === ESC_KEY) {
+          this.displayBlockFilter = false
+        }
+      })
     }
   }
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-  .filter-content {
-    margin-top: 15px;
-
-    button {
-      i.left {
-        margin-right: 8px;
-      }
-      padding-left: 10px;
-      padding-right: 10px;
-      margin-top: 6px;
-    }
-  }
-
   .quick-search {
     button {
       margin-top: 2px;
@@ -134,16 +160,87 @@
         font-size: 1.3rem;
         margin-left: 4px;
         margin-top: 13px;
+        color: grey;
         &.remove-search {
-          right: 48px;
+          right: 20px;
           cursor: pointer;
+          top: 0;
         }
+        &.search {
+          pointer-events: none;
+        }
+      }
+      a {
+        position: absolute;
+        right: 70px;
+        top: 50%;
+        transform: translateY(-50%);
+        text-decoration: underline;
       }
       input {
         height: 48px;
         padding-left: 34px;
         margin-bottom: 0;
-        width: 84%;
+        width: 100%;
+        padding-right: 215px;
+        box-sizing: border-box;
+      }
+    }
+  }
+
+  .open-search {
+    position: absolute;
+    z-index: 1001;
+    background-color: #fff;
+
+    i.close {
+      float: right;
+      font-size: 1.3em;
+      cursor: pointer;
+      margin-top: 5px;
+      color: grey;
+    }
+
+    .filter-content {
+      margin-top: 15px;
+
+      .dots {
+        border-left: 1px dotted rgba(0,0,0,0.26);
+        padding-bottom: 5px;
+      }
+      .block-and {
+        margin-bottom: 5px;
+        button {
+          i.left {
+            margin-right: 8px;
+          }
+          padding-left: 10px;
+          padding-right: 10px;
+          margin-top: 6px;
+        }
+      }
+      .block-sort {
+        margin-top: 30px;
+        margin-bottom: 0;
+      }
+      .block-content {
+        margin-left: 5px;
+      }
+      p {
+        i {
+          font-size: 1.1em;
+          margin-right: 10px;
+        }
+      }
+
+      .actions {
+        &.card-action {
+          padding: 10px;
+          button {
+            margin-top: 10px;
+            margin-right: 10px;
+          }
+        }
       }
     }
   }
