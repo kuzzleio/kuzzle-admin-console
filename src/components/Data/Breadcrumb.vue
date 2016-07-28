dceb3e521e2564edece8f9c8fb93af9222e68c6a
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-07-28 17:43:30 +0200

    add basic breadcrumb + fix bug watch data collection menu

---
+++ src/components/Data/Breadcrumb.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="subnav">
+    <ul>
+      <li>
+        DataBreadcrumb
+      </li>
+      <li v-link-active>
+        <!--<a v-link="{name: 'SummaryData', activeClass: 'active'}">Summary</a>-->
+      </li>
+    </ul>
+  </nav>
+
+</template>
+
+<style lang="scss" scoped>
+  .subnav {
+    position: fixed;
+    z-index: 100;
+    top: 50px;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'DataBreadcrumb'
+  }
+</script>
