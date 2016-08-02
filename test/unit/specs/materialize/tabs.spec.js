2711e15a9f0b50f70bf2e65a174dabd58ade7031
Anthony Sendra <asendra@kaliop.com>
2016-08-02 14:00:26 +0200

    unit test tab component

--- test/unit/specs/materialize/tabs.spec.js
+++ test/unit/specs/materialize/tabs.spec.js
@@ -84,25 +84,76 @@ describe('Tabs component', () => {
 
     describe('resizeIndicator', () => {
       it('should do nothing if there is no active tab', () => {
-        let right = vm.$refs.tabs.$els.indicator.style.right
+        vm.$refs.tabs.$els.indicator = {style: {right: '10px', left: '20px'}}
         vm.$refs.tabs.activeTab = null
 
         vm.$refs.tabs.resizeIndicator()
-        expect(vm.$refs.tabs.$els.indicator.style.right).to.be.equal(right)
+        expect(vm.$refs.tabs.$els.indicator.style.right).to.be.equal('10px')
+        expect(vm.$refs.tabs.$els.indicator.style.left).to.be.equal('20px')
       })
 
       it('should do nothing if tab or tabs width 0', () => {
+        vm.$refs.tabs.$els.indicator = {style: {right: '10px', left: '20px'}}
+        vm.$refs.tabs.tabsCount = 4
+        vm.$refs.tabs.activeTab = {index: 1, $el: {parentElement: {offsetWidth: 0, scrollWidth: 2}}}
+
+        vm.$refs.tabs.resizeIndicator()
+        expect(vm.$refs.tabs.$els.indicator.style.right).to.be.equal('10px')
+        expect(vm.$refs.tabs.$els.indicator.style.left).to.be.equal('20px')
       })
 
       it('should change right and left style', () => {
+        vm.$refs.tabs.$els.indicator = {style: {right: '10px', left: '20px'}}
+        vm.$refs.tabs.$set('$children', ['firstChild', 'secondChild'])
+        vm.$refs.tabs.activeTab = {index: 1, $el: {parentElement: {offsetWidth: 10, scrollWidth: 2}}}
 
+        vm.$refs.tabs.resizeIndicator()
+
+        expect(vm.$refs.tabs.$els.indicator.style.right).to.be.equal('0px')
+        expect(vm.$refs.tabs.$els.indicator.style.left).to.be.equal('5px')
       })
     })
 
     describe('moveIndicator', () => {
       it('should call velocity library with new right/left position', () => {
+        vm.$refs.tabs.moveIndicator(10, 20)
 
+        expect(velocitySpy.args[0][1]).to.be.deep.equal({right: 20, left: 10})
       })
+    })
+  })
+
+  describe('Ready', () => {
+    it('should call broadcast with current active tab', () => {
+      let Tabs = TabsInjector({
+        'velocity-animate': velocitySpy
+      })
+      document.body.insertAdjacentHTML('afterbegin', '<app></app>')
+      Vue.$broadcast = sandbox.spy()
+      vm = new Vue({
+        template: '<div><tabs v-ref:tabs active="something"></tabs></div>',
+        components: {
+          Tabs
+        }
+      }).$mount('app')
+
+      expect(Vue.$broadcast.calledWith('tab-select', 'something'))
+    })
+
+    it('should call broadcast with current active tab', () => {
+      let Tabs = TabsInjector({
+        'velocity-animate': velocitySpy
+      })
+      document.body.insertAdjacentHTML('afterbegin', '<app></app>')
+      Vue.$broadcast = sandbox.spy()
+      vm = new Vue({
+        template: '<div><tabs v-ref:tabs></tabs></div>',
+        components: {
+          Tabs
+        }
+      }).$mount('app')
+
+      expect(Vue.$broadcast.called).to.be.equal(false)
     })
   })
 })
