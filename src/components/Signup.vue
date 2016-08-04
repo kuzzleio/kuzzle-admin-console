46ded1a9e06da4399ffa800bfe5d0fd50c27dddc
Anthony Sendra <asendra@kaliop.com>
2016-08-04 18:20:51 +0200

    singup wip

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,54 +2,72 @@
   <div class="container">
     <div class="col m6">
       <h2 class="center-align">
-        <img src="../assets/logo.png" alt="Welcome to the Kuzzle Backoffice" />
+        <img src="../assets/logo.png" alt="Welcome to the Kuzzle Backoffice"/>
       </h2>
-      <div class="row">
+      <div class="row message-warning">
         <div class="col s6 offset-s3">
-          <div class="card-panel teal">
-          <span class="white-text ">
-            You're about to create your first admin!
-          </span>
+          <div class="card-panel primary">
+            <div class="white-text">
+              You're about to create your first admin account!
+            </div>
+            <div class="white-text">
+              <i class="fa fa-warning"></i>
+              If you want to secure your Kuzzle, you have to check the "reset" checkbox.
+              If you do so, be sure to configure default and anonymous roles afterward.
+            </div>
           </div>
         </div>
       </div>
       <div class="row">
-        <form class="col s6 offset-s3" id="loginForm" method="post" @submit.prevent="doLogin(username, password)">
+        <form class="col s6 offset-s3" id="loginForm" method="post" @submit.prevent="signup">
           <div class="row">
             <div class="input-field col s12">
-              <input id="username" v-model="username" type="text" name="username" required
+              <input id="username" v-model="username" type="text" name="username"
                      class="validate"/>
               <label for="username">Username</label>
             </div>
           </div>
           <div class="row">
             <div class="input-field col s12">
-              <input v-model="password1" type="password" name="password1" id="pass1" required
+              <input v-model="password1" type="password" name="password1" id="pass1"
                      class="validate"/>
               <label for="pass1">Password</label>
             </div>
           </div>
           <div class="row">
             <div class="input-field col s12">
-              <input v-model="password2" type="password" name="password2" id="pass2" required
+              <input v-model="password2" type="password" name="password2" id="pass2"
                      class="validate"/>
               <label for="pass2">Password (confirmation)</label>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s12">
-              <input v-model="reset" type="checkbox" class="filled-in" id="filled-in-box" />
-              <label for="filled-in-box">Reset default anonymous rights?</label>
+            <div class="input-field col s12 reset">
+              <input v-model="reset" type="checkbox" class="filled-in" id="reset"/>
+              <label for="reset">Reset default and anonymous rights?</label>
             </div>
           </div>
           <div class="row">
             <div class="col m6">
-              <p class="error">{{error}}</p>
+              <p class="message error">{{error}}</p>
             </div>
             <div class="col m6">
-              <p class="right-align">
-                <button class="btn waves-effect waves-light" type="submit" name="action">Create my first admin</button>
-              </p>
+              <div class="preloader-wrapper active right" v-if="waiting" >
+                <div class="spinner-layer">
+                  <div class="circle-clipper left">
+                    <div class="circle"></div>
+                  </div>
+                  <div class="gap-patch">
+                    <div class="circle"></div>
+                  </div>
+                  <div class="circle-clipper right">
+                    <div class="circle"></div>
+                  </div>
+                </div>
+              </div>
+              <button v-show="!waiting" class="btn waves-effect waves-light right" type="submit" name="action">
+                Create my first admin
+              </button>
             </div>
           </div>
         </form>
@@ -57,3 +75,58 @@
     </div>
   </div>
 </template>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  .row.message-warning {
+    margin-bottom: 0;
+  }
+
+  [type="checkbox"] + label {
+    line-height: 19px;
+  }
+
+  .row {
+    margin-bottom: 5px;
+  }
+
+  .reset.input-field {
+    margin-top: 0;
+  }
+
+  .message.error {
+    margin-top: 25px;
+  }
+</style>
+
+<script>
+  import kuzzle from '../services/kuzzle'
+
+  export default {
+    name: 'Signup',
+    data () {
+      return {
+        username: null,
+        password1: null,
+        password2: null,
+        reset: false,
+        error: null,
+        waiting: false
+      }
+    },
+    methods: {
+      signup () {
+        if (this.password1 !== this.password2) {
+          this.error = 'Password does not match'
+          return
+        }
+
+        this.error = null
+        this.waiting = true
+
+        kuzzle
+          .security
+          .createFirstAdminPromise({username: this.username, password: this.password1}, {reset: true})
+      }
+    }
+  }
+</script>
