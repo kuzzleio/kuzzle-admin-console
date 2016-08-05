a66b589e74ece34d57d7f4361e2913f96fcf73a3
jenow <jenova.ke20@gmail.com>
2016-08-05 18:02:40 +0200

    Removed SET_ERROR and UNSET_ERROR

--- test/unit/specs/auth/actions.spec.js
+++ test/unit/specs/auth/actions.spec.js
@@ -1,4 +1,4 @@
-import { testAction } from '../helper'
+import { testAction, testActionPromise } from '../helper'
 const actionsInjector = require('inject!../../../../src/vuex/modules/auth/actions')
 
 let triggerError = true
@@ -20,9 +20,10 @@ describe('doLogin action', () => {
   })
 
   it('should set an error on login', (done) => {
-    testAction(actions.doLogin, ['user', 'pwd'], {}, [
-      { name: 'SET_ERROR', payload: ['error'] }
-    ], done)
+    testActionPromise(actions.doLogin, ['user', 'pwd'], {}, [], done).catch(e => {
+      expect(e.message).to.equals('error')
+      done()
+    })
   })
 
   it('should login a user', (done) => {
