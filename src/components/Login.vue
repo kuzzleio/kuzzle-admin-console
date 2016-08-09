530bf09fb549029ded58e33b5254040a811817a6
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-08-09 19:42:25 +0200

    rework login form style

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,42 +1,66 @@
 <template>
-  <div class="container">
-    <div class="col m6">
-      <h2 class="center-align">
-        <img src="../assets/logo.png" alt="Welcome to the Kuzzle Backoffice" />
-      </h2>
+  <div class="login">
+    <div class="container">
       <div class="row">
-        <form class="col s6 offset-s3" id="loginForm" method="post" @submit.prevent="login(username, password)">
+        <div class="col card wrapper s10 offset-s1 m8 offset-m2 l4 offset-l4">
           <div class="row">
-            <div class="input-field col s12">
-              <input id="username" v-model="username" type="text" name="username" required
-                     class="validate"/>
-              <label for="username">Email</label>
+            <div class="col s12">
+              <h2 class="center-align logo">
+                <img src="../assets/logo.png" alt="Welcome to the Kuzzle Backoffice" style="width: 70%" />
+              </h2>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s12">
-              <input v-model="password" type="password" name="password" id="pass" required
-                     class="validate"/>
-              <label for="pass">Password</label>
-            </div>
+            <form class="col s10 offset-s1" id="loginForm" method="post" @submit.prevent="login(username, password)">
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="username" v-model="username" type="text" name="username" required tabindex="1"
+                         class="validate"/>
+                  <label for="username">Email</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input v-model="password" type="password" name="password" id="pass" required tabindex="2"
+                         class="validate"/>
+                  <label for="pass">Password</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col m6">
+                  <p class="error">{{error}}</p>
+                </div>
+                <div class="col m6">
+                  <p class="right-align">
+                    <button class="btn waves-effect waves-light" type="submit" name="action" tabindex="3">Login</button>
+                  </p>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="row">
-            <div class="col m6">
-              <p class="error">{{error}}</p>
-            </div>
-            <div class="col m6">
-              <p class="right-align">
-                <button class="btn waves-effect waves-light" type="submit" name="action">Login</button>
-              </p>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style type="text/css" media="screen" scoped>
+  .login {
+    position: fixed;
+    top:0;
+    left:0;
+    bottom: 0;
+    right: 0;
+    padding-top: 50px;
+    height: 100%;
+    background: linear-gradient(45deg, rgba(31,77,81,1) 0%,rgba(0,75,82,1) 22%,rgba(18,105,109,1) 75%,rgba(0,117,127,1) 100%);
+  }
+  #loginForm {
+    margin-top: 15px;
+  }
+  .logo {
+    margin-top: 60px;
+  }
   .error {
     color: #d54f58;
     font-size: 18px;
