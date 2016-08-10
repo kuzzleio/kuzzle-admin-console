72d1447f27893d35af6412325d6315b61f0ff9f3
Luca Marchesini <luca@kuzzle.io>
2016-08-10 17:19:54 +0200

    Moved highlight filter to component scope

--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ import App from './App'
 import store from './vuex/store'
 import { sync } from 'vuex-router-sync'
 import { loginFromCookie } from './vuex/modules/auth/actions'
-import { highlight } from './filters/highlight.filter'
 
 Vue.config.debug = process.env.NODE_ENV !== 'production'
-
-Vue.filter('highlight', highlight)
 
 sync(store, router)
 
