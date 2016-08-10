9607fd6400a6906df530e1050e1482725b059eed
Luca Marchesini <luca@kuzzle.io>
2016-08-10 16:12:08 +0200

    Added SDK JS 2 support (waiting for effective build to be published on npm)

--- src/services/kuzzle.js
+++ src/services/kuzzle.js
@@ -1,4 +1,20 @@
 import Kuzzle from 'kuzzle-sdk'
 import config from '../../config'
+import Promise from 'bluebird'
 
-export default new Kuzzle(`http://${config.backend.host}:${config.backend.port}`)
+Kuzzle.prototype.bluebird = Promise
+
+if (!window.WebSocket) {
+  console.warn('No WebSocket support found on current browser, falling back to Socket.io')
+  window.kuzzle = new Kuzzle(config.backend.host, {
+    connect: 'manual'
+  })
+  require(['socket.io-client'], function (socketio) {
+    window.io = socketio
+    window.kuzzle.connect()
+  })
+} else {
+  window.kuzzle = new Kuzzle(config.backend.host)
+}
+
+export default window.kuzzle
