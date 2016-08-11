c44ace147e741a9304e676a89ec1d5e35c011a46
Luca Marchesini <luca@kuzzle.io>
2016-08-11 17:09:05 +0200

    added io/ws conf and clean up docker-compose files

--- src/services/kuzzle.js
+++ src/services/kuzzle.js
@@ -1,4 +1,4 @@
-import Kuzzle from 'kuzzle-sdk'
+import Kuzzle from 'kuzzle-sdk/dist/kuzzle'
 import config from '../../config'
 import Promise from 'bluebird'
 
@@ -7,14 +7,19 @@ Kuzzle.prototype.bluebird = Promise
 if (!window.WebSocket) {
   console.warn('No WebSocket support found on current browser, falling back to Socket.io')
   window.kuzzle = new Kuzzle(config.backend.host, {
-    connect: 'manual'
+    connect: 'manual',
+    ioPort: config.backend.ioPort,
+    wsPort: config.backend.wsPort
   })
   require(['socket.io-client'], function (socketio) {
     window.io = socketio
     window.kuzzle.connect()
   })
 } else {
-  window.kuzzle = new Kuzzle(config.backend.host)
+  window.kuzzle = new Kuzzle(config.backend.host, {
+    ioPort: config.backend.ioPort,
+    wsPort: config.backend.wsPort
+  })
 }
 
 export default window.kuzzle
