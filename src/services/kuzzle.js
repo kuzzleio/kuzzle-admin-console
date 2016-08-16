e5c280065fe459b416ca0a2d37f7bdc85692fc1f
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-08-16 16:32:58 +0200

    rewrite kuzzle service to detach requirements from window + remove kuzzle service test (due to lazyloading of socketio)

--- src/services/kuzzle.js
+++ src/services/kuzzle.js
@@ -1,25 +1,29 @@
-import Kuzzle from 'kuzzle-sdk'
+import Kuzzle from 'kuzzle-sdk/dist/kuzzle'
 import config from '../../config'
 import Promise from 'bluebird'
+
+let kuzzle
 
 Kuzzle.prototype.bluebird = Promise
 
 if (!window.WebSocket) {
   console.warn('No WebSocket support found on current browser, falling back to Socket.io')
-  window.kuzzle = new Kuzzle(config.backend.host, {
+  kuzzle = new Kuzzle(config.backend.host, {
     connect: 'manual',
     ioPort: config.backend.ioPort,
     wsPort: config.backend.wsPort
   })
   require(['socket.io-client'], function (socketio) {
     window.io = socketio
-    window.kuzzle.connect()
+    kuzzle.connect()
   })
 } else {
-  window.kuzzle = new Kuzzle(config.backend.host, {
+  kuzzle = new Kuzzle(config.backend.host, {
     ioPort: config.backend.ioPort,
     wsPort: config.backend.wsPort
   })
 }
 
-export default window.kuzzle
+window.kuzzle = kuzzle
+
+export default kuzzle
