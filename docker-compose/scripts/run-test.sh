ec2766f9a81951603cc3015dcc416674f1df1c8d
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2016-10-18 10:32:18 +0200

    simpify travis run tests

--- docker-compose/scripts/run-test.sh
+++ docker-compose/scripts/run-test.sh
@@ -9,17 +9,4 @@ bower install --allow-root --no-interactive
 echo "[$(date --rfc-3339 seconds)] - Starting backoffice unit tests..."
 npm run unit
 
-echo "[$(date --rfc-3339 seconds)] - Starting backoffice codecov report..."
-npm run codecov
-
-if [ "${TRAVIS_BRANCH}" = "master" ]
-then
-    echo "[$(date --rfc-3339 seconds)] - Detected to be on master branch"
-    echo "[$(date --rfc-3339 seconds)] - Building dist file"
-    npm run build
-    echo "[$(date --rfc-3339 seconds)] - Creating archive for release"
-    tar -cvf kuzzle-backoffice.tar dist
-    chmod 777 kuzzle-backoffice.tar
-fi
-
 echo "[$(date --rfc-3339 seconds)] - Backoffice tests complete"
