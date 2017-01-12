53ebbc9353071b3c847c41290b67c49b02238234
Anthony Sendra <asendra@kaliop.com>
2017-01-12 17:16:27 +0100

    fix add object in object

--- src/components/Common/JsonForm/JsonForm.vue
+++ src/components/Common/JsonForm/JsonForm.vue
@@ -6,7 +6,7 @@
         {{name}}
         <i @click="addAttribute" class="fa fa-plus-circle primary"></i>
       </legend>
-      <div v-for="(nestedName, nestedContent) in content.properties">
+      <div v-for="(nestedContent, nestedName) in content.properties">
         <json-form :name="nestedName" :full-name-input="path" :content="nestedContent"></json-form>
       </div>
     </fieldset>
