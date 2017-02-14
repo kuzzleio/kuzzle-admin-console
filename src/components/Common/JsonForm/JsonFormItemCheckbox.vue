55dde4c8c06ce2213c0a215cf98d7b284f2af16d
Anthony Sendra <asendra@kaliop.com>
2017-02-14 18:35:50 +0100

    checkbox false in document

--- src/components/Common/JsonForm/JsonFormItemCheckbox.vue
+++ src/components/Common/JsonForm/JsonFormItemCheckbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row input-field">
-    <input :id="name" type="checkbox" name="name" v-model="value" :checked="content" />
+    <input :id="name" type="checkbox" name="name" @change="update" :checked="value" />
     <label :for="name">{{name}}</label>
   </div>
 </template>
@@ -9,7 +9,7 @@
   export default {
     name: 'JsonFormItemCheckbox',
     props: {
-      content: {type: Boolean, default: false},
+      content: Object,
       name: String,
       type: String,
       parent: String
@@ -22,14 +22,18 @@
     methods: {
       initValue () {
         if (this.parent) {
-          this.value = this.content[this.parent][this.name]
+          this.value = this.content[this.parent][this.name] || false
         } else {
-          this.value = this.content[this.name]
+          this.value = this.content[this.name] || false
         }
+      },
+      update (e) {
+        this.$emit('update-value', {name: this.name, value: e.target.checked})
       }
     },
     mounted () {
       this.initValue()
+      this.$emit('update-value', {name: this.name, value: this.value})
     },
     watch: {
       content: 'initValue'
