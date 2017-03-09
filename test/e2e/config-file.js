e1701fd04ea2a8cd0e687996bebb1ac9654a66ee
jenow <kblondel@kaliop.com>
2017-03-09 10:09:15 +0100

    Secure keys

--- test/e2e/config-file.js
+++ test/e2e/config-file.js
@@ -1,6 +1,6 @@
 exports.config = {
-  project: "YG2S7yvx",
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6ImNpOllHMlM3eXZ4IiwiZW1haWwiOiJqZW5vdmEua2UyMEBnbWFpbC5jb20ifSwiaWF0IjoxNDg3NjY2MjU4fQ.x5nLn_vXkYQyUuiNdB85b6SpShP-rwDjXpkpT6d5wjU",
+  project: process.env.TESTIM_PROJECT,
+  token: process.env.TESTIM_TOKEN,
   host: "hub",
   "report-file": "testim-report.xml",
   label: "main",
