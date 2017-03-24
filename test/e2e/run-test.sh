1c1fb9b1d7d6e5dbaa8371621c34197e1a74c636
Anthony Sendra <asendra@kaliop.com>
2017-03-24 11:14:30 +0100

    test connecting travis

--- test/e2e/run-test.sh
+++ test/e2e/run-test.sh
@@ -1,2 +1,9 @@
-testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label firstAdmin --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
-testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label normal --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdkbG07a/Fqrrh65BlY+wE458wyneyFSdQ+Pe+UR/t8v8ZDKdT+spsjyxKVUxzOk46zIRlq3v1eMvlPcjCI/kjk+SEu347U5qG/BUly6tNONEF1XytBBB7YLBG+YGfufWGzex6ZbkYdQxDiheefBC/P5K1OvT0WcOMiqzBOrnIk7Xs2rtkI3aiJ/ncgTgjnN0sAFrQRPtTgUuhsuJWLUr2OHTmH/w5Q76onZjp7rpnl3Ad8j6P0JCmlFcQL6pp6vt9ZimYGsZbrblPXFBo38yH+kUF9qgUpkV6Nw5psJxC09qP8hmK2j9AfcIIwSNRtO0f97pKjDAGa6BE/gw9dqzJ asendra@kd-5Z4KS12" >> ~/.ssh/authorized_keys
+
+curl https://ifconfig.co/
+
+tail -f /dev/null
+
+#testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label firstAdmin --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
+#testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label normal --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
+
