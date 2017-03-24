350b388322bf010662c9f5e6392bdf2fda9801c9
Anthony Sendra <asendra@kaliop.com>
2017-03-24 10:20:57 +0100

    rename test with normal label

--- test/e2e/run-test.sh
+++ test/e2e/run-test.sh
@@ -1,2 +1,2 @@
 KUZZLE_ENV=proxy testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label firstAdmin --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
-KUZZLE_ENV=proxy testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label standard --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
+KUZZLE_ENV=proxy testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label normal --base-url http://backoffice --report-file testim-report.xml --config-file /opt/config-file.js
