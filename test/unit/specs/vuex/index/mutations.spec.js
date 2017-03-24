b731324a9a1f1b814046698f96e33769c6bb3eb3
Anthony Sendra <asendra@kaliop.com>
2017-03-24 17:20:40 +0100

    fix unit test

--- test/unit/specs/vuex/index/mutations.spec.js
+++ test/unit/specs/vuex/index/mutations.spec.js
@@ -1,15 +1,10 @@
 import { mutations } from '../../../../../src/vuex/modules/index/store'
-
-const {
+import {
   ADD_INDEX,
   ADD_STORED_COLLECTION,
   ADD_REALTIME_COLLECTION,
   RECEIVE_INDEXES_COLLECTIONS,
-  RECEIVE_MAPPING,
-  SET_PARTIAL_TO_DOCUMENT,
-  SET_NEW_DOCUMENT,
-  UNSET_NEW_DOCUMENT,
-  DELETE_INDEX} = mutations
+  DELETE_INDEX} from '../../../../../src/vuex/modules/index/mutation-types'
 
 describe('Data mutation', () => {
   describe('ADD_INDEX test', () => {
@@ -19,7 +14,7 @@ describe('Data mutation', () => {
         indexesAndCollections: {}
       }
 
-      ADD_INDEX(state, 'myindex')
+      mutations[ADD_INDEX](state, 'myindex')
       expect(state.indexes).to.deep.equal(['myindex'])
       expect(state.indexesAndCollections['myindex']).to.deep.equal({
         stored: [],
@@ -43,24 +38,24 @@ describe('Data mutation', () => {
     })
 
     it('should add a stored collection', () => {
-      ADD_STORED_COLLECTION(state, {index: 'foo2', name: 'bar'})
+      mutations[ADD_STORED_COLLECTION](state, {index: 'foo2', name: 'bar'})
       expect(state.indexesAndCollections['foo2']).to.deep.equal({stored: ['bar'], realtime: []})
       expect(state.indexes).to.deep.equals(['foo', 'foo2'])
     })
 
     it('should not add a stored collection if it already exists', () => {
-      ADD_STORED_COLLECTION(state, {index: 'foo', name: 'bar'})
+      mutations[ADD_STORED_COLLECTION](state, {index: 'foo', name: 'bar'})
       expect(state.indexesAndCollections['foo']).to.deep.equal({stored: ['bar'], realtime: []})
     })
 
     it('should add a realtime collection', () => {
-      ADD_REALTIME_COLLECTION(state, {index: 'foo2', name: 'bar'})
+      mutations[ADD_REALTIME_COLLECTION](state, {index: 'foo2', name: 'bar'})
       expect(state.indexesAndCollections['foo2']).to.deep.equal({stored: [], realtime: ['bar']})
       expect(state.indexes).to.deep.equals(['foo', 'foo2'])
     })
 
     it('should not add a realtime collection if it already exists', () => {
-      ADD_REALTIME_COLLECTION(state, {index: 'foo', name: 'bar'})
+      mutations[ADD_REALTIME_COLLECTION](state, {index: 'foo', name: 'bar'})
       expect(state.indexesAndCollections['foo']).to.deep.equal({stored: [], realtime: ['bar']})
     })
   })
@@ -69,46 +64,8 @@ describe('Data mutation', () => {
     it('should set indexes and collections', () => {
       let state = {user: null}
 
-      RECEIVE_INDEXES_COLLECTIONS(state, {foo: {realtime: ['toto'], stored: ['tutu']}})
+      mutations[RECEIVE_INDEXES_COLLECTIONS](state, {foo: {realtime: ['toto'], stored: ['tutu']}})
       expect(state.indexesAndCollections).to.deep.equal({foo: {realtime: ['toto'], stored: ['tutu']}})
-    })
-  })
-
-  describe('RECEIVE_MAPPING tests', () => {
-    it('should set mapping', () => {
-      let state = {mapping: null}
-
-      RECEIVE_MAPPING(state, 'mapping')
-      expect(state.mapping).to.equals('mapping')
-    })
-  })
-
-  describe('SET_PARTIAL_TO_DOCUMENT test', () => {
-    it('should make an object from a path', () => {
-      let state = {newDocument: {}}
-
-      SET_PARTIAL_TO_DOCUMENT(state, {path: 'foo.bar', value: 'baz'})
-      expect(state.newDocument).to.deep.equals({foo: {bar: 'baz'}})
-      SET_PARTIAL_TO_DOCUMENT(state, {path: 'oof', value: 'barr'})
-      expect(state.newDocument).to.deep.equals({foo: {bar: 'baz'}, oof: 'barr'})
-    })
-  })
-
-  describe('SET_NEW_DOCUMENT test', () => {
-    it('should set a new document', () => {
-      let state = {newDocument: {}}
-
-      SET_NEW_DOCUMENT(state, {foo: {bar: 'baz'}})
-      expect(state.newDocument).to.deep.equals({foo: {bar: 'baz'}})
-    })
-  })
-
-  describe('UNSET_NEW_DOCUMENT tests', () => {
-    it('should unset the new document', () => {
-      let state = {newDocument: {foo: 'bar'}}
-
-      UNSET_NEW_DOCUMENT(state)
-      expect(state.newDocument).to.deep.equals({})
     })
   })
 
@@ -124,7 +81,7 @@ describe('Data mutation', () => {
         }
       }
 
-      DELETE_INDEX(state, 'foo')
+      mutations[DELETE_INDEX](state, 'foo')
       expect(state.indexesAndCollections).to.deep.equal({})
     })
   })
