48c97248fe5accf4db8e40084670961d579e0280
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-04-03 11:57:44 +0200

    enable fast finish in e2e tests

--- test/e2e/run-test.sh
+++ test/e2e/run-test.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 while ! curl -f -s -o /dev/null "http://backoffice:3000"
 do
-    echo "[$(date --rfc-3339 seconds)] - Waiting for http://backoffice:3000\n"
+    echo "[$(date --rfc-3339 seconds)] - Waiting for http://backoffice:3000"
     sleep 5
 done
+
+set -e
 
 testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label firstAdmin --base-url http://backoffice:3000 --report-file testim-report.xml --config-file /opt/config-file.js
 testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label normal --base-url http://backoffice:3000 --report-file testim-report.xml --config-file /opt/config-file.js
