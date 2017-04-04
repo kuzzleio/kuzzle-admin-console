8080dae91ed0c9b599084993e81fc47ca8d51359
Anthony Sendra <asendra@kaliop.com>
2017-04-04 16:11:05 +0200

    enable BO static files

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -64,11 +64,7 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+        loader: 'url-loader'
       }
     ]
   },
