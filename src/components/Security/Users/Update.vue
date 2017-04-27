992aef32a1492632bf709f393b978a7137f8650e
jenow <kblondel@kaliop.com>
2017-04-27 17:03:09 +0200

    replace createOrReplaceUser to updateUser

--- src/components/Security/Users/Update.vue
+++ src/components/Security/Users/Update.vue
@@ -53,7 +53,7 @@
 
         kuzzle
           .security
-          .createUserPromise(decodeURIComponent(this.$store.state.route.params.id), user, {replaceIfExist: true})
+          .updateUserPromise(decodeURIComponent(this.$store.state.route.params.id), user)
           .then(() => {
             setTimeout(() => { // we can't perform refresh index on %kuzzle
               this.$router.push({name: 'SecurityUsersList'})
