058c2a64004866f58a73906725e6328df034b5ef
Anthony Sendra <asendra@kaliop.com>
2017-07-03 10:50:36 +0200

    add testim securityUser

--- test/e2e/run-test.sh
+++ test/e2e/run-test.sh
@@ -10,4 +10,4 @@ set -e
 
 testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label firstAdmin --base-url http://backoffice:3000 --report-file testim-report.xml --config-file /opt/config-file.js
 testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label normal --base-url http://backoffice:3000 --report-file testim-report.xml --config-file /opt/config-file.js
-
+testim --token $TESTIM_TOKEN --project $TESTIM_PROJECT --host "hub" --browser $BROWSER --label securityUser --base-url http://backoffice:3000 --report-file testim-report.xml --config-file /opt/config-file.js
