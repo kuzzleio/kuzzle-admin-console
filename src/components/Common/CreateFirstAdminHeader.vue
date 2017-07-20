da5d34a230c5bedc3c037c5adc753df9b4c1a7ff
Luca Marchesini <luca@kuzzle.io>
2017-07-20 16:12:36 +0200

    Decorations

--- src/components/Common/CreateFirstAdminHeader.vue
+++ src/components/Common/CreateFirstAdminHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="create-first-admin-header">
-    <span><b>Warning!</b> Your Kuzzle has no administrator user. It is strongly recommended
-    <router-link :to="{name: 'Signup'}"> that you create one.</router-link></span>
+    <span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> <b>Warning!</b> Your Kuzzle has no administrator user. It is strongly recommended
+    <router-link :to="{name: 'Signup'}"> that you create one.</router-link></span> <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
   </div>
 </template>
