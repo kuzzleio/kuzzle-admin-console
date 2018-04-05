9c8dcbdc85586cdb92e2b4cd93758821f2db35ed
njuelle <n.juelle@gmail.com>
2018-04-05 10:28:00 +0200

    add better error message in 404 component

--- src/components/404.vue
+++ src/components/404.vue
@@ -2,6 +2,7 @@
   <div class="wrapper">
     <div class="row center">
       <h1>404 not found</h1>
+      <p>The index or collection that you requested doesn't exist.</p>
       <a href="#">Go back to the main page</a>
     </div>
   </div>
