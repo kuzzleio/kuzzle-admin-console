5318f0e8bbdfd128bfe14ab7d0ba33eb6b87377b
yvan stern <dev@appacea.com>
2018-06-19 10:41:55 -0400

    Further grammar corrections and changed bin name.

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -6,7 +6,7 @@
           <div class="row">
             <div class="col s12">
               <h2 class="center-align logo">
-                <img src="../assets/logo.svg" alt="Welcome to the Kuzzle Backoffice" style="width: 60%" />
+                <img src="../assets/logo.svg" alt="Welcome to the Kuzzle Admin Console" style="width: 60%" />
               </h2>
             </div>
           </div>
