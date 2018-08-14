f576bcb48b03ea2194a14d8fcf236da7dd6ca148
Nicolas Juelle <n.juelle@gmail.com>
2018-08-14 10:26:22 +0200

    add bind methods in jsonFormatter directive (#429)

--- src/directives/json-formatter.directive.js
+++ src/directives/json-formatter.directive.js
@@ -7,11 +7,16 @@ export default {
       binding.value.content,
       binding.value.open ? Infinity : 0
     ).render()
-    if (!el.innerHTML) {
-      el.appendChild(html)
-    } else {
-      el.innerHTML = ''
-      el.appendChild(html)
-    }
+
+    el.innerHTML = ''
+    el.appendChild(html)
+  },
+  bind(el, binding) {
+    const html = new JSONFormatter(
+      binding.value.content,
+      binding.value.open ? Infinity : 0
+    ).render()
+
+    el.appendChild(html)
   }
 }
