ff65e7375d6adaf98d25405b5ec2e08545299895
Adrien Maret <amaret93@gmail.com>
2018-08-29 14:30:26 +0200

    KZL 461 - Display warning when using https version of admin console (#440)
    * Display warning when using https version of admin console
    
    * Display warning if ssl is unchecked + fix bug
    
    * Update for PR

---
+++ src/components/Common/WarningHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="WarningHeader">
+    <span v-html="text" />    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'WarningHeader',
+    props: {
+      text: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.WarningHeader {
+  line-height: 2.5em;
+  background-color: $blue-color;
+  color: #fff;
+  text-align: center;
+
+  a {
+    color: #fff;
+    text-decoration: underline;
+  }
+}
+</style>
