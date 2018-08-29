ae98701082a33574013bcf60656af837afdb3662
Luca Marchesini <xbill82@gmail.com>
2018-08-29 08:57:06 +0200

    Shrinked vertical layout of initial pages (#445)
    * Now remembering list view in currentFilter
    
    * Preventing event emission if button is disabled
    
    * Shrinked a little bit the vertical layout of signup and createEnvironment page

--- src/components/Common/Environments/CreateEnvironmentPage.vue
+++ src/components/Common/Environments/CreateEnvironmentPage.vue
@@ -3,14 +3,9 @@
     <div class="container">
       <div class="row">
         <div class="col card wrapper s10 offset-s1 m8 offset-m2 l6 offset-l3">
-          <div class="row">
-            <div class="col s12">
-              <h2 class="center-align logo">
-                <img src="../../../assets/logo.svg" alt="Welcome to the Kuzzle Admin Console" style="width: 70%" />
-              </h2>
-            </div>
-          </div>
-
+          <h2 class="center-align logo">
+            <img src="../../../assets/logo.svg" alt="Welcome to the Kuzzle Admin Console" style="width: 70%" />
+          </h2>
           <div class="row message-warning">
             <h5>Create a Connection</h5>
             <div class="divider"></div>
@@ -37,22 +32,22 @@
 </template>
 
 <script>
-  import CreateEnvironment from './CreateEnvironment'
+import CreateEnvironment from './CreateEnvironment'
 
-  export default {
-    name: 'CreateEnvironmentPage',
-    components: {
-      CreateEnvironment
-    },
-    methods: {
-      createEnvironment () {
-        try {
-          this.$refs.createEnvironmentComponent.createEnvironment()
-            .then(() => this.$router.push({name: 'Home'}))
-            .catch((e) => console.error(e))
-        } catch (e) {
-        }
-      }
+export default {
+  name: 'CreateEnvironmentPage',
+  components: {
+    CreateEnvironment
+  },
+  methods: {
+    createEnvironment() {
+      try {
+        this.$refs.createEnvironmentComponent
+          .createEnvironment()
+          .then(() => this.$router.push({ name: 'Home' }))
+          .catch(e => console.error(e))
+      } catch (e) {}
     }
   }
+}
 </script>
