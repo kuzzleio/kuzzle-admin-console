c647c4edde84a147a05e8042bae65ecba95e377b
Aschen (Adrien Maret) <amaret93@gmail.com>
2019-02-19 11:30:20 +0100

    fix test cy

--- test/e2e/cypress/integration/collections.spec.js
+++ test/e2e/cypress/integration/collections.spec.js
@@ -28,6 +28,7 @@ describe('Collection management', function() {
   it('is able to create a realtime collection and access it', function() {
     cy.visit('/')
     cy.get('.LoginAsAnonymous-Btn').click()
+    cy.contains('testindex')
     cy.visit(`/#/data/${indexName}/create`)
 
     cy.get('.Mapping-name')
