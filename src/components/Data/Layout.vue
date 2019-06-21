c983de930a0cb4a685dd44a0507ea51657d0126b
jenow <jenova.ke20@gmail.com>
2019-06-21 11:11:43 +0200

    only fetch indexes and collections when adding or removing one

--- src/components/Data/Layout.vue
+++ src/components/Data/Layout.vue
@@ -24,7 +24,7 @@ import { LIST_INDEXES_AND_COLLECTION } from '../../vuex/modules/index/mutation-t
 import { FETCH_COLLECTION_DETAIL } from '../../vuex/modules/collection/mutation-types'
 import Treeview from './Leftnav/Treeview'
 import NotFound from '../404'
-import { SET_TOAST } from '../../vuex/modules/common/toaster/mutation-types'
+// import { SET_TOAST } from '../../vuex/modules/common/toaster/mutation-types'
 
 export default {
   name: 'DataLayout',
@@ -35,22 +35,6 @@ export default {
   data() {
     return {
       routeExist: true
-    }
-  },
-  watch: {
-    $route() {
-      if (canSearchIndex()) {
-        try {
-          this.$store.dispatch(LIST_INDEXES_AND_COLLECTION)
-          this.setRouteExist()
-          return this.$store.dispatch(FETCH_COLLECTION_DETAIL, {
-            index: this.$route.params.index,
-            collection: this.$route.params.collection
-          })
-        } catch (err) {
-          this.$store.commit(SET_TOAST, { text: err.message })
-        }
-      }
     }
   },
   mounted() {
