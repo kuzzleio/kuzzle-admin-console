11b9ac39587458de76d4d5ccab722c6f5982eaa5
jenow <jenova.ke20@gmail.com>
2019-06-21 11:49:38 +0200

    fix

--- src/components/Data/Layout.vue
+++ src/components/Data/Layout.vue
@@ -24,6 +24,7 @@ import { LIST_INDEXES_AND_COLLECTION } from '../../vuex/modules/index/mutation-t
 import { FETCH_COLLECTION_DETAIL } from '../../vuex/modules/collection/mutation-types'
 import Treeview from './Leftnav/Treeview'
 import NotFound from '../404'
+import { SET_TOAST } from '../../vuex/modules/common/toaster/mutation-types'
 
 export default {
   name: 'DataLayout',
@@ -34,6 +35,21 @@ export default {
   data() {
     return {
       routeExist: true
+    }
+  },
+  watch: {
+    $route() {
+      if (canSearchIndex()) {
+        try {
+          this.setRouteExist()
+          return this.$store.dispatch(FETCH_COLLECTION_DETAIL, {
+            index: this.$route.params.index,
+            collection: this.$route.params.collection
+          })
+        } catch (err) {
+          this.$store.commit(SET_TOAST, { text: err.message })
+        }
+      }
     }
   },
   mounted() {
