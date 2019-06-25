352358199156e4b3bda11dd8ac7d04be3cef385b
jenow <jenova.ke20@gmail.com>
2019-06-25 15:59:17 +0200

    update vue-quill-editor dependencie

--- src/components/Common/JsonForm/JsonFormItemRichEditor.vue
+++ src/components/Common/JsonForm/JsonFormItemRichEditor.vue
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 import { quillEditor } from 'vue-quill-editor'
 
 export default {
