5ca4889dffb822b3fc91ebefcfb015ce3700ff43
jenow <jenova.ke20@gmail.com>
2019-11-04 14:58:08 +0100

    fix tests

--- test/e2e/cypress/integration/users.spec.js
+++ test/e2e/cypress/integration/users.spec.js
@@ -83,7 +83,7 @@ describe('Users', function() {
     cy.contains('Indexes')
     cy.visit('/#/security/users')
 
-    cy.get('[id="checkbox-dummy"]').click({force: true})
+    cy.get('[id="checkbox-dummy"]').click({ force: true })
     cy.get('.BulkActions')
       .contains('Delete')
       .click()
@@ -101,18 +101,16 @@ describe('Users', function() {
     cy.contains('Indexes')
     cy.visit('/#/security/users/custom-mapping')
 
-    cy
-    .get('#user-custom-data-mapping-editor .ace_line')
-    .should('be.visible')
+    cy.get('#user-custom-data-mapping-editor .ace_line').should('be.visible')
 
     cy.get('#user-custom-data-mapping-editor .ace_line')
       .contains('{')
       .click({ force: true })
     cy.get('textarea.ace_text-input')
-    .clear({force: true})
-    .type(`{{}"address": {{}"type": "text"},`, {
-      force: true
-    })
+      .clear({ force: true })
+      .type(`{{}"address": {{}"type": "text"},`, {
+        force: true
+      })
 
     cy.get('.UserCustomMappingEditor-submit').click()
     cy.contains('User Management')
@@ -134,6 +132,6 @@ describe('Users', function() {
       force: true
     })
     cy.get('.UserCustomMappingEditor-submit').click()
-    cy.contains('Field "name" already has a mapping, and it cannot be changed')
+    cy.contains('Caught an unexpected error: name. keyword text')
   })
 })
