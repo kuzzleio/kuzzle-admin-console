a829c62e2e801f9c24e090b7bde7044379f1037b
jenow <jenova.ke20@gmail.com>
2019-12-03 14:50:33 +0100

    wip

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,9 +3,7 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-var webpackConfig = require('../../build/webpack.test.conf')
-
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
@@ -14,23 +12,13 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
-    files: [
-      '../../node_modules/babel-polyfill/dist/polyfill.js',
-      './index.js'
-    ],
+    files: ['../../node_modules/babel-polyfill/dist/polyfill.js', './index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
-    webpack: webpackConfig,
-    webpackMiddleware: {
-      noInfo: true
-    },
     coverageReporter: {
       dir: './coverage',
-      reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'lcov', subdir: '.' }, { type: 'text-summary' }]
     }
   })
 }
