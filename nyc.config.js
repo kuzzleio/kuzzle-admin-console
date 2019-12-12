3916b831b717770a24295de92cadbfee471b3e75
jenow <jenova.ke20@gmail.com>
2019-12-12 11:25:59 +0100

    update nyc config

--- nyc.config.js
+++ nyc.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'check-coverage': false,
+  'check-coverage': true,
   'per-file': true,
   'skip-full': true,
   all: true,
   include: ['src/services/*.js', 'src/vuex/**/*.js'],
   exclude: ['src/*.js', '**/index.js'],
   reporter: ['lcov', 'text', 'text-summary'],
-  extension: ['.js', '.vue']
+  extension: ['.js']
 }
