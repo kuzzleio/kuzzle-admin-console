91210f30f8078e605cb1cb7f682129b897f99db7
Luca Marchesini <xbill82@gmail.com>
2020-02-28 10:59:35 +0100

    better loadEnvironments logic

--- src/vuex/modules/common/kuzzle/store.ts
+++ src/vuex/modules/common/kuzzle/store.ts
@@ -187,10 +187,20 @@ const actions = createActions({
     try {
       loadedEnv = JSON.parse(localStorage.getItem(LS_ENVIRONMENTS) || '{}')
       commit.setEnvironments(loadedEnv)
+    } catch (e) {
+      // eslint-disable-next-line
+      console.error(e)
+      commit.setEnvironments({})
+    }
+
+    try {
       currentId = localStorage.getItem(LS_CURRENT_ENV)
       commit.setCurrentEnvironment(currentId)
-    } catch (e) {
-      commit.setEnvironments({})
+    } catch (error) {
+      // eslint-disable-next-line
+      console.warn(
+        `Something went wrong while setting the current environment, ${error.message}`
+      )
     }
   }
 })
