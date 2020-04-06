e63f4931df65cedfc7d6e1c46c464effd61fb02d
Aschen <adrien@maret.email>
2020-04-06 13:51:57 +0200

    fix collection list max size

--- src/vuex/modules/index/actions.js
+++ src/vuex/modules/index/actions.js
@@ -30,10 +30,11 @@ export default {
     result = result.filter(index => index !== '%kuzzle')
     for (const index of result) {
       try {
-        const res = await Vue.prototype.$kuzzle
-          .collection
-          .list(index)
-      
+        const res = await Vue.prototype.$kuzzle.collection.list(
+          index,
+          { size: 42000 }
+        )
+
         let collections = splitRealtimeStoredCollections(res.collections)
 
         if (!collections.realtime) {
