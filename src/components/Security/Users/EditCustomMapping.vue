deba2307123e51ac5bd9aabac7e0aa4911594b27
Berthier <Berthier.est@gmail.com>
2020-04-22 15:12:31 +0200

    omit

--- src/components/Security/Users/EditCustomMapping.vue
+++ src/components/Security/Users/EditCustomMapping.vue
@@ -90,7 +90,7 @@ import {
   getMappingUsers,
   updateMappingUsers
 } from '../../../services/kuzzleWrapper'
-import { omit } from 'lodash'
+import omit from 'lodash'
 
 export default {
   name: 'UsersCustomMappingWizard',
