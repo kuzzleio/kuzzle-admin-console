3b8393d18029468bed577051191d0934dfe90163
Aschen <adrien@maret.email>
2020-05-04 10:39:49 +0200

    Add comment

--- src/components/Data/Documents/DocumentListItem.vue
+++ src/components/Data/Documents/DocumentListItem.vue
@@ -123,7 +123,11 @@ export default {
     checkboxId() {
       return `checkbox-${this.document.id}`
     },
-    formatedDocument () {
+    /**
+     * Deletes the "id" who should not be displayed in the document body.
+     * Also put the "_kuzzle_info" field in last position
+     */
+    formattedDocument () {
       const document = _.omit(this.document, ['id', '_kuzzle_info'])
       document._kuzzle_info = this.document._kuzzle_info
       return document
