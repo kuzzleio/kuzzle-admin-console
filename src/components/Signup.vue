c3d4ad8264963925d43af9a1eec1929ca1222061
Berthier <Berthier.est@gmail.com>
2020-10-29 10:33:56 +0100

    add v4 banner

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -13,6 +13,11 @@
             />
           </h2>
           <div class="row">
+            <div class="col offset-s1 s10">
+              <warning-header :text="bannerV4Text" />
+            </div>
+          </div>
+          <div class="row">
             <div class="col offset-s4 s2">
               <environment-switch
                 @environment::create="editEnvironment"
@@ -136,11 +141,13 @@
 
 <script>
 import EnvironmentSwitch from './Common/Environments/EnvironmentsSwitch'
+import WarningHeader from './Common/WarningHeader'
 
 export default {
   name: 'Signup',
   components: {
-    EnvironmentSwitch
+    EnvironmentSwitch,
+    WarningHeader
   },
   data() {
     return {
@@ -149,7 +156,9 @@ export default {
       password2: '',
       reset: false,
       error: null,
-      waiting: false
+      waiting: false,
+      bannerV4Text:
+        'Hey! A new version of the admin console is available <a target="_blank" href="http://next-console.kuzzle.io">here</a> you should try it!'
     }
   },
   methods: {
