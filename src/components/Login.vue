504ab5a0a2e6d7c08ac23b25ee2a2e86e0978adc
Berthier <Berthier.est@gmail.com>
2020-10-30 10:59:38 +0100

    add beta keyword

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -68,7 +68,7 @@ export default {
   data() {
     return {
       bannerV4Text:
-        '<span>Hey! A new version of the admin console is available <a target="_blank" href="http://next-console.kuzzle.io">here</a>.<br> You should try it!</span>',
+        '<span>Hey! A new beta version of the admin console is available <a target="_blank" href="http://next-console.kuzzle.io">here</a>.<br> You should try it!</span>',
       environmentId: null
     }
   },
