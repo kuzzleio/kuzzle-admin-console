8326960ba4cb0cf9742be2ce9ea68a5533e7a9c6
Druesne Morgan <morgan.druesne@epitech.eu>
2020-11-17 11:27:58 +0100

    add button to revoke anonymous role right

--- src/components/Security/Roles/Page.vue
+++ src/components/Security/Roles/Page.vue
@@ -7,6 +7,13 @@
       <b-col class="text-right mt-3">
         <b-button
           class="mr-2"
+          data-cy="RolesManagement-revokeAnonymous"
+          variant="primary"
+          v-b-modal.modal-revokeAnonymous
+          >Revoke anonymous rights</b-button
+        >
+        <b-button
+          class="mr-2"
           data-cy="RolesManagement-createBtn"
           variant="primary"
           :disabled="!canCreateRole"
@@ -26,14 +33,21 @@
     >
       <b-card class="EmptyState text-center" slot="emptySet">
         <i class="text-secondary fas fa-unlock-alt fa-6x mb-3"></i>
-        <h2 class="text-secondary font-weight-bold">
-          No role is defined
-        </h2>
+        <h2 class="text-secondary font-weight-bold">No role is defined</h2>
         <p class="text-secondary" v-if="canCreateRole">
           You can create a new role by hitting the button above
         </p>
       </b-card>
     </role-list>
+    <b-modal
+      id="modal-revokeAnonymous"
+      title="Revoke anonymous rights"
+      @ok="revokeAnonymous"
+    >
+      <p class="my-4">
+        Do you want to revoke the rights of the annonymous role ?
+      </p>
+    </b-modal>
   </b-container>
 </template>
 
@@ -50,7 +64,59 @@ export default {
     RoleList,
     Headline
   },
+  methods: {
+    revokeAnonymous () {
+      this.$kuzzle.security.updateRole('anonymous', {
+        controllers: {
+          "*": {
+            "actions": {
+              "*": false
+            }
+          },
+          auth: {
+            actions: {
+              checkToken: true,
+              getCurrentUser: true,
+              getMyRights: true,
+              login: true
+            }
+          },
+          server: {
+            actions: {
+              publicApi: true,
+              openapi: true
+            }
+          }
+        }
+      });
+      this.$kuzzle.security.updateRole('default', {
+        controllers: {
+          "*": {
+            "actions": {
+              "*": false
+            }
+          },
+          auth: {
+            actions: {
+              checkToken: true,
+              getCurrentUser: true,
+              getMyRights: true,
+              logout: true,
+              updateSelf: true
+            }
+          },
+          server: {
+            actions: {
+              publicApi: true
+            }
+          }
+        }
+      });
+      this.$router.go(this.$router.currentRoute)
+    },
+  },
   computed: {
+    ...mapGetters('kuzzle', ['$kuzzle']),
     ...mapGetters('auth', ['canSearchRole', 'canCreateRole'])
   }
 }
