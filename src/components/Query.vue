a9cc3b93981513ba72419b5e58b782b6326bc47b
Berthier <Berthier.est@gmail.com>
2020-12-30 11:54:42 +0100

    toasts display error status colors ui ux

--- src/components/Query.vue
+++ src/components/Query.vue
@@ -1,32 +1,76 @@
 <template>
   <Multipane class="DataLayout Custom-resizer" layout="vertical">
     <div class="DataLayout-sidebarWrapper" data-cy="DataLayout-sidebarWrapper">
-      <b-button @click="resetFields">
-        New Query
-      </b-button>
-      <b-button @click="importQuery">
-        Import Query
-      </b-button>
-      <b-list-group>
-        <b-list-group-item
-          v-for="(query, index) of savedQueries"
-          class="d-flex justify-content-between align-items-center"
-          :key="`saved-query-${index}`"
-          :active="index === currentQueryIndex"
-        >
-          <span @click="loadSavedQuery(index)">{{ query.name }}</span>
-          <i :class="`fas fa-trash`" @click="deleteSavedQuery(index)" />
-        </b-list-group-item>
-      </b-list-group>
+      <b-row class="h-100 text-center mx-0" align-v="center" align-h="center">
+        <b-col cols="12" class="h-100 px-0">
+          <b-card class="h-100 backgroundCard" no-body>
+            <b-card-body class="d-flex flex-column text-center m-0 p-0">
+              <b-row class="mx-0">
+                <b-col class="my-3">
+                  <b-button @click="resetFields" variant="primary">
+                    <i class="fas fa-plus mr-2" />
+                    New Query
+                  </b-button>
+                </b-col>
+              </b-row>
+              <b-card-text class="px-1">
+                <b-list-group>
+                  <b-list-group-item
+                    v-for="query of paginedQueries"
+                    class="d-flex justify-content-between align-items-center"
+                    :key="`saved-query-${query.idx}`"
+                    :active="query.idx === currentQueryIndex"
+                  >
+                    <span @click="loadSavedQuery(query.idx)">{{
+                      query.name
+                    }}</span>
+                    <i
+                      :class="`fas fa-trash`"
+                      @click="deleteSavedQuery(query.idx)"
+                    />
+                  </b-list-group-item>
+                </b-list-group>
+              </b-card-text>
+              <b-row class="mt-auto mx-0" align-h="center">
+                <b-pagination
+                  v-model="currentPage"
+                  :total-rows="savedQueries.length"
+                  :per-page="queryPerPage"
+                  aria-controls="my-table"
+                ></b-pagination>
+              </b-row>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
     </div>
     <MultipaneResizer data-cy="sidebarResizer" />
     <div class="DataLayout-contentWrapper">
       <b-container fluid class="queryContainer">
-        <b-row>
+        <b-row align-v="stretch">
+          <b-col cols="12">
+            <b-alert show variant="info">
+              Here, you'll be able to perform custom
+              <a
+                href="https://docs.kuzzle.io/sdk/js/7/core-classes/kuzzle/query/"
+                target="_blank"
+                >query
+                <i class="fa fa-external-link-alt" />
+              </a>
+              to Kuzzle following the
+              <a
+                href="https://docs.kuzzle.io/core/2/api/payloads/request/"
+                target="_blank"
+                >API Documentation
+                <i class="fa fa-external-link-alt" />
+              </a>
+              .
+            </b-alert>
+          </b-col>
           <b-col cols="7">
             <b-card no-body>
               <b-card-header>
-                <b-card-title>
+                <b-card-title >
                   Query
                 </b-card-title>
               </b-card-header>
@@ -43,52 +87,77 @@
                   reference="queryEditor"
                   tabindex="4"
                   myclass="h-100"
-                  :height="650"
+                  :height="600"
                   :content="currentQuery"
                   @change="jsonQueryChanged"
                 />
               </b-card-text>
               <b-card-footer>
-                <b-button @click="saveCurrentQuery" :disabled="!isQueryValid">
-                  SAVE
-                </b-button>
-                <b-button @click="performQuery" :disabled="!isQueryValid">
-                  RUN
-                </b-button>
-                <b-button @click="exportQuery" :disabled="!isQueryValid">
-                  EXPORT
-                </b-button>
-                <b-button @click="copyQuery">
-                  COPY
-                </b-button>
+                <b-row>
+                  <b-col class="text-left">
+                    <b-button
+                      @click="performQuery"
+                      :disabled="!isQueryValid"
+                      variant="success"
+                    >
+                      <i class="fas fa-rocket mr-2" />
+                      RUN
+                    </b-button>
+                  </b-col>
+                  <b-col class="text-right">
+                    <b-button
+                      @click="saveCurrentQuery"
+                      :disabled="!isQueryValid"
+                      variant="outline-primary"
+                      class="mr-3"
+                    >
+                      <i class="fas fa-save mr-2" />
+                      SAVE
+                    </b-button>
+
+                    <b-button
+                      @click="copyToClipBoard('Query')"
+                      variant="outline-secondary"
+                    >
+                      <i class="fas fa-copy mr-2" />
+                      COPY
+                    </b-button>
+                  </b-col>
+                </b-row>
               </b-card-footer>
             </b-card>
           </b-col>
           <b-col cols="5">
-            <b-card no-body>
+            <b-card no-body class="h-100">
               <b-card-header>
                 <b-card-title>
                   Response
                 </b-card-title>
               </b-card-header>
-              <b-card-text v-if="!loading" class="mb-0">
-                <b-row class="m-3"> Status: {{ currentStatus }} </b-row>
+              <b-card-body class="p-0" v-if="!loading">
+                <b-row class="m-2" no-gutters>
+                  <b-col cols="12">
+                    <b-alert show :variant="statusBarVariant" class="mb-0">
+                      Status: {{ currentStatus }}
+                    </b-alert>
+                  </b-col>
+                </b-row>
                 <json-editor
                   id="response"
                   ref="responseEditorWrapper"
                   reference="responseEditor"
                   tabindex="4"
-                  myclass="h-100"
+                  :height="603"
                   readonly
-                  :height="650"
                   :content="currentResponse"
                 />
-              </b-card-text>
-              <b-card-footer>
-                <b-button @click="exportResponse">
-                  EXPORT
-                </b-button>
-                <b-button @click="copyResponse">
+              </b-card-body>
+              <b-card-footer class="text-right">
+                <b-button
+                  @click="copyToClipBoard('Response')"
+                  variant="outline-secondary"
+                >
+                  <i class="fas fa-copy mr-2" />
                   COPY
                 </b-button>
               </b-card-footer>
@@ -119,11 +188,31 @@ export default {
       currentStatus: null,
       savedQueries: [],
       loading: true,
-      currentQueryIndex: null
+      currentQueryIndex: null,
+      queryPerPage: 10,
+      currentPage: 1
     }
   },
   computed: {
     ...mapGetters('kuzzle', ['wrapper', 'currentEnvironment']),
+    paginedQueries() {
+      return this.savedQueries
+        .map((q, index) => {
+          q.idx = index
+          return q
+        })
+        .filter(q => {
+          return (
+            q.idx >= this.currentPage * this.queryPerPage - this.queryPerPage &&
+            q.idx < this.currentPage * this.queryPerPage
+          )
+        })
+    },
+    statusBarVariant() {
+      if (this.currentStatus === null) return 'secondary'
+      if (this.currentStatus.toString().match(/20[0-9]/)) return 'success'
+      return 'danger'
+    },
     isQueryValid() {
       if (!this.currentQuery) {
         return false
@@ -137,13 +226,43 @@ export default {
     }
   },
   methods: {
-    copyQuery() {},
-    exportResponse() {},
-    copyResponse() {},
+    copyToClipBoard(type) {
+      const textarea = document.createElement('textarea')
+      if (type === 'Response') {
+        textarea.value = this.currentResponse
+      } else if (type === 'Query') {
+        textarea.value = this.currentQuery
+      }
+      textarea.setAttribute('readonly', '')
+      document.body.appendChild(textarea)
+
+      textarea.select()
+      try {
+        document.execCommand('copy')
+        this.$bvToast.toast(`${type} successfully copied.`, {
+          title: 'Success',
+          variant: 'success',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
+      } catch (err) {
+        this.$bvToast.toast(`Failed to copy ${type}.`, {
+          title: 'Error',
+          variant: 'danger',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
+      }
+      document.body.removeChild(textarea)
+    },
     isQueryNameValid() {
       if (!this.currentQueryName) {
-        console.error('empty name')
-        //todo toast error empty name
+        this.$bvToast.toast('Empty query name.', {
+          title: 'Error',
+          variant: 'danger',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
         return false
       }
       const nameSearch = this.savedQueries.find((q, index) => {
@@ -154,8 +273,12 @@ export default {
       })
       const nameAlreadyUsed = nameSearch !== undefined
       if (nameAlreadyUsed) {
-        console.error('name already used')
-        // todo error toast name already used
+        this.$bvToast.toast('Query name already used.', {
+          title: 'Error',
+          variant: 'danger',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
       }
       return !nameAlreadyUsed
     },
@@ -167,8 +290,12 @@ export default {
     },
     saveCurrentQuery() {
       if (!this.isQueryValid) {
-        console.error('invalid query')
-        //todo error toast i[${this.currentQueryIndex}]
+        this.$bvToast.toast('Invalid query.', {
+          title: 'Error',
+          variant: 'danger',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
       }
       if (!this.isQueryNameValid()) {
         return
@@ -193,6 +320,12 @@ export default {
       }
       storedQueries[this.currentEnvironment] = this.savedQueries
       localStorage.setItem('savedQueries', JSON.stringify(storedQueries))
+      this.$bvToast.toast('Query successfully saved.', {
+        title: 'Success',
+        variant: 'success',
+        toaster: 'b-toaster-bottom-right',
+        appendToast: true
+      })
     },
     deleteSavedQuery(index) {
       //todo toast confirm
@@ -206,29 +339,47 @@ export default {
       }
       storedQueries[this.currentEnvironment] = this.savedQueries
       localStorage.setItem('savedQueries', JSON.stringify(storedQueries))
+      this.$bvToast.toast(`Query successfully deleted.`, {
+        title: 'Success',
+        variant: 'success',
+        toaster: 'b-toaster-bottom-right',
+        appendToast: true
+      })
     },
     async performQuery() {
-      try {
-        const response = await this.wrapper.query(JSON.parse(this.currentQuery))
-        console.log(response)
-        this.currentResponse = JSON.stringify(response, null, ' ')
-        this.currentStatus = response.status
-        this.$refs.responseEditorWrapper.setContent(this.currentResponse)
-        //toast here
-      } catch (error) {
-        console.log(error)
-        // display toast here
-        // display error in response
+      const response = await this.wrapper.query(JSON.parse(this.currentQuery))
+      this.currentResponse = JSON.stringify(response, null, ' ')
+      this.currentStatus = response.status
+      this.$refs.responseEditorWrapper.setContent(this.currentResponse)
+      if (this.statusBarVariant === 'danger') {
+        this.$bvToast.toast('An error occured while playing query.', {
+          title: 'Error',
+          variant: 'danger',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
+      } else {
+        this.$bvToast.toast('Query successfully played.', {
+          title: 'Success',
+          variant: 'success',
+          toaster: 'b-toaster-bottom-right',
+          appendToast: true
+        })
       }
     },
     jsonQueryChanged(val) {
       this.currentQuery = val
     },
-    importQuery() {},
-    exportQuery() {},
     resetFields() {
-      this.currentQuery = '{}'
-      this.$refs.queryEditorWrapper.setContent('{}')
+      this.currentQuery = `{
+  "controller": "",
+  "action": "",
+  "index": "",
+  "collection": "",
+  "_id": "",
+  "body": {}
+}`
+      this.$refs.queryEditorWrapper.setContent(this.currentQuery)
       this.currentQueryName = ''
       this.currentResponse = ''
       this.currentStatus = null
@@ -244,18 +395,13 @@ export default {
   mounted() {
     this.loading = true
     this.loadStoredQueries()
-    this.currentQuery = `
-{
-  "controller": "document",
-  "action": "create",
-  "index": "nyc-open-data",
-  "collection": "yellow-taxi",
-  "_id": "my-custom-document-id",
-  "refresh": "wait_for",
-  "body": {
-    "trip_distance": 4.23,
-    "passenger_count": 2
-  }
+    this.currentQuery = `{
+  "controller": "",
+  "action": "",
+  "index": "",
+  "collection": "",
+  "_id": "",
+  "body": {}
 }`
     this.loading = false
   }
@@ -263,14 +409,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+::v-deep .card-title {
+  margin-bottom: 0px;
+}
+
 .DataLayout {
   height: 100%;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
 }
-.DataLayout-sidebarWrapper {
+
+.backgroundCard {
   background-color: $light-grey-color;
+}
+
+.DataLayout-sidebarWrapper {
   min-width: $sidebar-width;
   width: $sidebar-width;
   height: 100%;
