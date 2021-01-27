ed434dc287029144fb9c21d550b512435b5a0db9
njuelle <n.juelle@gmail.com>
2021-01-27 11:17:34 +0100

    add dynamic ref props with default value

--- src/components/Common/JsonEditor.vue
+++ src/components/Common/JsonEditor.vue
@@ -1,7 +1,8 @@
 <template>
   <div
+    :name="id"
     data-cy="JSONEditor"
-    ref="jsoneditor"
+    :ref="id"
     :class="classes"
     :id="id"
     :style="style"
@@ -26,70 +27,77 @@
 </style>
 
 <script>
-import Vue from 'vue'
-let editor
+import Vue from 'vue';
 
 export default {
   name: 'JsonEditor',
   props: {
     content: String,
+    id: {
+      type: String,
+      default: Date.now().toString() + Math.random().toString()
+    },
     myclass: {
       type: String,
       default: ''
     },
     readonly: Boolean,
-    id: String,
     height: { type: Number, default: 250 }
+  },
+  data() {
+    return {
+      editor: null
+    };
   },
   computed: {
     classes() {
-      return (this.readonly ? 'readonly ' : '') + this.myclass
+      return (this.readonly ? 'readonly ' : '') + this.myclass;
     },
     style() {
       if (this.height === undefined) {
-        return { 'min-height': '250px' }
+        return { 'min-height': '250px' };
       } else {
-        return { 'min-height': this.height + 'px!important' }
+        return { 'min-height': this.height + 'px!important' };
       }
     }
   },
   methods: {
     getRawValue() {
-      return editor.getValue()
+      return this.editor.getValue();
     },
     getEditor() {
-      return editor
+      return this.editor;
     },
     setContent(value) {
-      this.$log.debug('Setting content', value)
-      editor.getSession().setValue(value)
+      this.$log.debug('Setting content', value);
+      this.editor.getSession().setValue(value);
     }
   },
   mounted() {
     Vue.nextTick(() => {
       /* eslint no-undef: 0 */
-      editor = ace.edit(this.$refs.jsoneditor, {
+      this.editor = ace.edit(this.$refs[this.id], {
         mode: 'ace/mode/json'
-      })
-      editor.setTheme('ace/theme/tomorrow')
-      editor.setFontSize(15)
-      editor.getSession().setTabSize(2)
-      editor.setReadOnly(this.readonly)
-      editor.$blockScrolling = Infinity
-      this.setContent(this.content)
+      });
+      this.editor.setTheme('ace/theme/tomorrow');
+      this.editor.setFontSize(15);
+      this.editor.getSession().setTabSize(2);
+      this.editor.setReadOnly(this.readonly);
+      this.editor.$blockScrolling = Infinity;
+      this.setContent(this.content);
 
       // WARNING - Beware of update loops!
       // This event is triggered both when the content changes after
       // user interaction and when it is set programmatically.
-      editor.on('change', () => {
-        this.$emit('change', this.getRawValue())
-      })
-    })
+      this.editor.on('change', () => {
+        this.$emit('change', this.getRawValue());
+      });
+    });
   },
   beforeDestroy() {
-    if (editor) {
-      editor.removeAllListeners('change')
+    if (this.editor) {
+      this.editor.removeAllListeners('change');
     }
   }
-}
+};
 </script>
