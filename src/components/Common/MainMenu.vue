05b332261a2e3e73d20d692b480c83ed5667f3cd
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2021-10-27 17:42:13 +0200

    Redirect to Login page if token expired (#968)
    * redirect to Login page if token expired
    
    * Apply suggestion: remove Toast on logout action failure
    
    * Refactor DoLogout try/catch
    
    * Add scenario to test the fix
    
    * boyscout: use security:revokeTokens to test properly token expiration
    
    * revert boyscout
    
    * Specify admin kuid in the new test

--- src/components/Common/MainMenu.vue
+++ src/components/Common/MainMenu.vue
@@ -147,20 +147,10 @@ export default {
     async doLogout() {
       try {
         await this.$store.direct.dispatch.auth.doLogout()
-        this.$router.push({ name: 'Login' })
       } catch (error) {
         this.$log.error(error)
-        this.$bvToast.toast(
-          'The complete error has been printed to the console.',
-          {
-            title: 'Ooops! Something went wrong while logging out.',
-            variant: 'warning',
-            toaster: 'b-toaster-bottom-right',
-            appendToast: true,
-            dismissible: true,
-            noAutoHide: true
-          }
-        )
+      } finally {
+        this.$router.push({ name: 'Login' })
       }
     },
 
